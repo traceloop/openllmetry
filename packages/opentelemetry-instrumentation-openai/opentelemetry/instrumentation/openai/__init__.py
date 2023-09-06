ccc33b3e4f83fb960e4a6a8fcce1e0f310045229
Gal Kleinman <gal.klm@gmail.com>
2023-09-06 15:27:30 +0300

    fix: imports & missing code components

--- packages/opentelemetry-instrumentation-openai/opentelemetry/instrumentation/openai/__init__.py
+++ packages/opentelemetry-instrumentation-openai/opentelemetry/instrumentation/openai/__init__.py
@@ -13,7 +13,7 @@ from opentelemetry.instrumentation.utils import (
     unwrap,
 )
 
-from traceloop.semconv import SpanAttributes, LLMRequestTypeValues
+from opentelemetry.semconv.llm import SpanAttributes, LLMRequestTypeValues
 
 logger = logging.getLogger(__name__)
 
@@ -42,9 +42,11 @@ def _set_span_attribute(span, name, value):
 
 
 def _set_api_attributes(span):
-    _set_span_attribute(span, SpanAttributes.OPENAI_API_BASE, openai.api_base)
-    _set_span_attribute(span, SpanAttributes.OPENAI_API_TYPE, openai.api_type)
-    _set_span_attribute(span, SpanAttributes.OPENAI_API_VERSION, openai.api_version)
+    _set_span_attribute(span, OpenAISpanAttributes.OPENAI_API_BASE, openai.api_base)
+    _set_span_attribute(span, OpenAISpanAttributes.OPENAI_API_TYPE, openai.api_type)
+    _set_span_attribute(
+        span, OpenAISpanAttributes.OPENAI_API_VERSION, openai.api_version
+    )
 
     return
 
@@ -192,6 +194,12 @@ def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
                 span.set_status(Status(StatusCode.OK))
 
         return response
+
+
+class OpenAISpanAttributes:
+    OPENAI_API_VERSION = "openai.api_version"
+    OPENAI_API_BASE = "openai.api_base"
+    OPENAI_API_TYPE = "openai.api_type"
 
 
 class OpenAIInstrumentor(BaseInstrumentor):
