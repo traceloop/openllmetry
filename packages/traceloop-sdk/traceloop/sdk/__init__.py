e571472e0e47e58d2c30b592d979f98361e38940
Gal Kleinman <gal.klm@gmail.com>
2023-09-07 23:36:47 +0300

    fix sdk and add more flows to sample app

--- packages/traceloop-sdk/traceloop/sdk/__init__.py
+++ packages/traceloop-sdk/traceloop/sdk/__init__.py
@@ -1,8 +1,14 @@
 from typing import Optional
-from traceloop.sdk.tracing import Tracing
+from traceloop.sdk.tracing.tracing import TracerWrapper, set_correlation_id
 
 
 class Traceloop:
+    __tracer_wrapper: TracerWrapper
+
     @staticmethod
     def init(app_name: Optional[str] = None) -> None:
-        Tracing.init(app_name=app_name)
+        Traceloop.__tracer_wrapper = TracerWrapper()
+
+    @staticmethod
+    def set_correlation_id(correlation_id: str) -> None:
+        set_correlation_id(correlation_id)
