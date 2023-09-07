8571c8fb443ad0e2ff7f189a26b8de0788b99d51
Gal Kleinman <33281963+galkleinman@users.noreply.github.com>
2023-09-07 11:08:04 +0300

    fix: traceloop sdk initialization and initial versions release for instrumentations (#7)

--- packages/traceloop-sdk/traceloop/sdk/__init__.py
+++ packages/traceloop-sdk/traceloop/sdk/__init__.py
@@ -1,1 +1,8 @@
-"""Traceloop Software Development Kit (SDK) for Python"""
+from typing import Optional
+from traceloop.sdk.tracing import Tracing
+
+
+class Traceloop:
+    @staticmethod
+    def init(app_name: Optional[str] = None) -> None:
+        Tracing.init(app_name=app_name)
