a29cf6c686485c338a1d702da02466d7616883b9
Nir Gazit <nir.gzt@gmail.com>
2023-09-09 15:24:37 +0200

    feat: auto-create link to traceloop dashboard

--- packages/traceloop-sdk/traceloop/sdk/__init__.py
+++ packages/traceloop-sdk/traceloop/sdk/__init__.py
@@ -1,5 +1,13 @@
+import os
+import requests
 from typing import Optional
+
+from colorama import Fore
+from opentelemetry.util.re import parse_env_headers
+
 from traceloop.sdk.tracing.tracing import TracerWrapper, set_correlation_id
+
+TRACELOOP_API_ENDPOINT = "https://api.traceloop.dev"
 
 
 class Traceloop:
@@ -7,6 +15,37 @@ class Traceloop:
 
     @staticmethod
     def init(app_name: Optional[str] = None) -> None:
+        api_key = os.getenv("TRACELOOP_API_KEY")
+        api_endpoint = os.getenv("TRACELOOP_API_ENDPOINT") or TRACELOOP_API_ENDPOINT
+        headers = os.getenv("TRACELOOP_HEADERS") or {}
+        if isinstance(headers, str):
+            headers = parse_env_headers(headers)
+
+        # auto-create a dashboard on Traceloop if no export endpoint is provided
+        if api_endpoint == TRACELOOP_API_ENDPOINT and api_key is None:
+            if os.path.exists("traceloop_key.txt"):
+                api_key = open("traceloop_key.txt").read()
+            else:
+                print(
+                    "No Traceloop API key provided, auto-creating a dashboard on Traceloop"
+                )
+                res = requests.post(
+                    "https://app.traceloop.com/api/registration/auto-create"
+                )
+                api_key = res.json()["apiKey"]
+                open("traceloop_key.txt", "w").write(api_key)
+            print(
+                Fore.GREEN
+                + f"Go to https://app.traceloop.com/trace?orgToken={api_key} to see a live dashboard"
+            )
+            print(Fore.RESET)
+
+        if api_key:
+            headers = {
+                "Authorization": f"Bearer {api_key}",
+            } | headers
+
+        TracerWrapper.set_endpoint(api_endpoint, headers)
         Traceloop.__tracer_wrapper = TracerWrapper()
 
     @staticmethod
