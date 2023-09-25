c670419bf2d5600a6481c8d6dd6d537de00b893e
Nir Gazit <nirga@users.noreply.github.com>
2023-09-24 19:16:34 -0700

    feat: Pinecone Instrumentation (#3)

--- packages/traceloop-sdk/tests/conftest.py
+++ packages/traceloop-sdk/tests/conftest.py
@@ -1,3 +1,18 @@
 """Unit tests configuration module."""
+import pytest
+from traceloop.sdk import Traceloop
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 
 pytest_plugins = []
+
+
+@pytest.fixture(scope="session")
+def exporter():
+    exporter = InMemorySpanExporter()
+    Traceloop.init(app_name="test", disable_batch=True, exporter=exporter)
+    return exporter
+
+
+@pytest.fixture(autouse=True)
+def clear_exporter(exporter):
+    exporter.clear()
