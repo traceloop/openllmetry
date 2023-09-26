ebe85fa23514500de9e0bb8c5abb87e8d01c5f02
Nir Gazit <nirga@users.noreply.github.com>
2023-09-25 17:54:37 -0700

    feat: Anthropic instrumentation (#71)

---
+++ packages/traceloop-sdk/tests/test_anthropic_instrumentation.py
@@ -0,0 +1,29 @@
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+from traceloop.sdk.decorators import workflow, task
+
+
+def test_anthropic_completion(exporter):
+    @task(name="joke_creation")
+    def create_joke():
+        client = Anthropic()
+        response = client.completions.create(
+            prompt=f"{HUMAN_PROMPT}\nHello world\n{AI_PROMPT}",
+            model="claude-instant-1.2",
+            max_tokens_to_sample=2048,
+            top_p=0.1,
+        )
+        print(response)
+        return response
+
+    @workflow(name="pirate_joke_generator")
+    def joke_workflow():
+        create_joke()
+
+    joke_workflow()
+
+    spans = exporter.get_finished_spans()
+    assert [span.name for span in spans] == [
+        "anthropic.completion",
+        "joke_creation.task",
+        "pirate_joke_generator.workflow",
+    ]
