af7fd0ce2f932d7458be1692c7b69a1511fec128
Gal Kleinman <gal.klm@gmail.com>
2023-09-26 11:36:26 -0700

    remove dummy test files

--- packages/opentelemetry-instrumentation-anthropic/tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-"""Hello unit test module."""
-
-from opentelemetry_instrumentation_anthropic.hello import hello
-
-
-def test_hello():
-    """Test the hello function."""
-    assert hello() == "Hello opentelemetry-instrumentation-anthropic"
--- packages/opentelemetry-instrumentation-weaviate/tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-"""Hello unit test module."""
-
-from opentelemetry_instrumentation_weaviate.hello import hello
-
-
-def test_hello():
-    """Test the hello function."""
-    assert hello() == "Hello opentelemetry-instrumentation-weaviate"
