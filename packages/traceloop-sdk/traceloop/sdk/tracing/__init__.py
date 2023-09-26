c5ae307bfc2c2faf6a278f2e0b8d0d567e319cda
Gal Kleinman <gal.klm@gmail.com>
2023-09-26 11:55:44 -0700

    ignore unused imports on __init__.py

--- packages/traceloop-sdk/traceloop/sdk/tracing/__init__.py
+++ packages/traceloop-sdk/traceloop/sdk/tracing/__init__.py
@@ -1,3 +1,2 @@
-from traceloop.sdk.tracing.context_manager import get_tracer as get_tracer
-from traceloop.sdk.tracing.tracing import set_workflow_name as set_workflow_name
-from traceloop.sdk.tracing.tracing import set_correlation_id as set_correlation_id
+from traceloop.sdk.tracing.context_manager import get_tracer
+from traceloop.sdk.tracing.tracing import set_workflow_name, set_correlation_id
