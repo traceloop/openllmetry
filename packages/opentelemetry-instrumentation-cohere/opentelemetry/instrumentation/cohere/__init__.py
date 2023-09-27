d73249edfdd3f5e86e72514e1baada5a6d2c5b91
Nir Gazit <nir.gzt@gmail.com>
2023-09-27 16:56:16 -0700

    feat: cohere instrumentation

--- packages/opentelemetry-instrumentation-cohere/opentelemetry/instrumentation/cohere/__init__.py
+++ packages/opentelemetry-instrumentation-cohere/opentelemetry/instrumentation/cohere/__init__.py
@@ -1,1 +1,152 @@
 """OpenTelemetry Cohere instrumentation"""
+import logging
+from typing import Collection
+from wrapt import wrap_function_wrapper
+
+from opentelemetry import context as context_api
+from opentelemetry.trace import get_tracer, SpanKind
+from opentelemetry.trace.status import Status, StatusCode
+
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+from opentelemetry.instrumentation.utils import (
+    _SUPPRESS_INSTRUMENTATION_KEY,
+    unwrap,
+)
+
+from opentelemetry.semconv.ai import SpanAttributes, LLMRequestTypeValues
+
+logger = logging.getLogger(__name__)
+
+_instruments = ("cohere >= 4.2.7",)
+__version__ = "0.1.0"
+
+WRAPPED_METHODS = [
+    {
+        "object": "Client",
+        "method": "generate",
+        "span_name": "cohere.completion",
+    },
+]
+
+
+def _set_span_attribute(span, name, value):
+    if value is not None:
+        if value != "":
+            span.set_attribute(name, value)
+    return
+
+
+def _set_input_attributes(span, kwargs):
+    _set_span_attribute(span, SpanAttributes.LLM_REQUEST_MODEL, kwargs.get("model"))
+    _set_span_attribute(
+        span, SpanAttributes.LLM_REQUEST_MAX_TOKENS, kwargs.get("max_tokens_to_sample")
+    )
+    _set_span_attribute(span, SpanAttributes.LLM_TEMPERATURE, kwargs.get("temperature"))
+    _set_span_attribute(span, SpanAttributes.LLM_TOP_P, kwargs.get("top_p"))
+    _set_span_attribute(
+        span, SpanAttributes.LLM_FREQUENCY_PENALTY, kwargs.get("frequency_penalty")
+    )
+    _set_span_attribute(
+        span, SpanAttributes.LLM_PRESENCE_PENALTY, kwargs.get("presence_penalty")
+    )
+
+    _set_span_attribute(
+        span, f"{SpanAttributes.LLM_PROMPTS}.0.user", kwargs.get("prompt")
+    )
+
+    return
+
+
+def _set_span_completions(span, completion):
+    index = 0
+    prefix = f"{SpanAttributes.LLM_COMPLETIONS}.{index}"
+    _set_span_attribute(
+        span, f"{prefix}.finish_reason", completion.get("finish_reason")
+    )
+    _set_span_attribute(span, f"{prefix}.content", completion.get("completion"))
+
+
+def _set_response_attributes(span, response):
+    _set_span_attribute(span, SpanAttributes.LLM_RESPONSE_MODEL, response.get("model"))
+    _set_span_completions(span, response)
+
+
+def _with_tracer_wrapper(func):
+    """Helper for providing tracer for wrapper functions."""
+
+    def _with_tracer(tracer, to_wrap):
+        def wrapper(wrapped, instance, args, kwargs):
+            return func(tracer, to_wrap, wrapped, instance, args, kwargs)
+
+        return wrapper
+
+    return _with_tracer
+
+
+@_with_tracer_wrapper
+def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
+    """Instruments and calls every function defined in TO_WRAP."""
+    if context_api.get_value(_SUPPRESS_INSTRUMENTATION_KEY):
+        return wrapped(*args, **kwargs)
+
+    name = to_wrap.get("span_name")
+    with tracer.start_as_current_span(
+        name,
+        kind=SpanKind.CLIENT,
+        attributes={
+            SpanAttributes.LLM_VENDOR: "Cohere",
+            SpanAttributes.LLM_REQUEST_TYPE: LLMRequestTypeValues.COMPLETION,
+        },
+    ) as span:
+        try:
+            if span.is_recording():
+                _set_input_attributes(span, kwargs)
+
+        except Exception as ex:  # pylint: disable=broad-except
+            logger.warning(
+                "Failed to set input attributes for cohere span, error: %s", str(ex)
+            )
+
+        response = wrapped(*args, **kwargs)
+
+        if response:
+            try:
+                if span.is_recording():
+                    _set_response_attributes(span, response)
+
+            except Exception as ex:  # pylint: disable=broad-except
+                logger.warning(
+                    "Failed to set response attributes for openai span, error: %s",
+                    str(ex),
+                )
+            if span.is_recording():
+                span.set_status(Status(StatusCode.OK))
+
+        return response
+
+
+class CohereInstrumentor(BaseInstrumentor):
+    """An instrumentor for Cohere's client library."""
+
+    def instrumentation_dependencies(self) -> Collection[str]:
+        return _instruments
+
+    def _instrument(self, **kwargs):
+        tracer_provider = kwargs.get("tracer_provider")
+        tracer = get_tracer(__name__, __version__, tracer_provider)
+        for wrapped_method in WRAPPED_METHODS:
+            wrap_object = wrapped_method.get("object")
+            wrap_method = wrapped_method.get("method")
+            wrap_function_wrapper(
+                "cohere.client",
+                f"{wrap_object}.{wrap_method}",
+                _wrap(tracer, wrapped_method),
+            )
+
+    def _uninstrument(self, **kwargs):
+        for wrapped_method in WRAPPED_METHODS:
+            wrap_object = wrapped_method.get("object")
+            unwrap(
+                f"cohere.client.{wrap_object}",
+                wrapped_method.get("method"),
+            )
