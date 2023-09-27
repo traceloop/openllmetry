6d27474c418dc44bd245b372d01a42b4fb6c62fa
Tomer <4tomer.friedman@gmail.com>
2023-09-27 14:44:48 -0700

    half the test

--- packages/traceloop-sdk/tests/test_workflows.py
+++ packages/traceloop-sdk/tests/test_workflows.py
@@ -25,3 +25,45 @@ def test_simple_workflow(exporter):
         "joke_creation.task",
         "pirate_joke_generator.workflow",
     ]
+
+
+def test_open_ai_function_calls(exporter):
+    @task(name="function_call_test")
+    def function_call_test():
+        completion = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "user", "content": "What's the weather like in Boston?"}
+            ],
+            functions=[
+                {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather in a given location",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "unit": {
+                                "type": "string",
+                                "enum": ["celsius", "fahrenheit"],
+                            },
+                        },
+                        "required": ["location"],
+                    },
+                }
+            ],
+            function_call="auto",
+        )
+        return completion.choices[0].message.content
+
+    @workflow(name="function_call_test_workflow")
+    def function_call_test_workflow():
+        function_call_test()
+
+    function_call_test_workflow()
+    spans = exporter.get_finished_spans()
+
+    # TODO logical assertion
