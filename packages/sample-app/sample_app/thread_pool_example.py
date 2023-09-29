81067cc728d375c8f4326355ccc8f97fbea4b1e1
Nir Gazit <nir.gzt@gmail.com>
2023-09-29 17:24:44 +0200

    docs: documented how to use with thread pools

---
+++ packages/sample-app/sample_app/thread_pool_example.py
@@ -0,0 +1,36 @@
+import pinecone
+from traceloop.sdk import Traceloop
+from traceloop.sdk.decorators import workflow
+from concurrent.futures import ThreadPoolExecutor
+import contextvars, functools
+
+Traceloop.init("thread_pool_repro")
+
+
+@workflow("test_thread_pool")
+def do_retrieval(index: pinecone.Index):
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        for _ in range(3):
+            # Note: this is needed instead of calling `submit` directly, like this:
+            # executor.submit(index.query, [1.0, 2.0, 3.0], top_k=10)
+            ctx = contextvars.copy_context()
+            executor.submit(
+                ctx.run,
+                functools.partial(index.query, [1.0, 2.0, 3.0], top_k=10),
+            )
+
+
+def get_index():
+    INDEX_NAME = "thread-pool-repro"
+    if INDEX_NAME not in pinecone.list_indexes():
+        pinecone.create_index(name=INDEX_NAME, dimension=3, metric="dotproduct")
+    return pinecone.Index(INDEX_NAME)
+
+
+def main():
+    index = get_index()
+    do_retrieval(index)
+
+
+if __name__ == "__main__":
+    main()
