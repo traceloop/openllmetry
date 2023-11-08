9c4803ce4881474f8fdc5ee5b04b0ccc95b02cdb
Gal Kleinman <gal.klm@gmail.com>
2023-11-08 11:28:19 +0200

    fix version + revert app openai usage

--- packages/sample-app/sample_app/methods_decorated_app.py
+++ packages/sample-app/sample_app/methods_decorated_app.py
@@ -1,15 +1,16 @@
-from openai import OpenAI
+import openai
+import os
 
 from traceloop.sdk import Traceloop
 from traceloop.sdk.decorators import task, agent, workflow, tool
 
-client = OpenAI()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 Traceloop.init(app_name="joke_generation_service")
 
 
 @task(name="joke_creation")
 def create_joke():
-    completion = client.chat.completions.create(
+    completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
     )
@@ -19,7 +20,7 @@ def create_joke():
 
 @agent(name="joke_translation")
 def translate_joke_to_pirate(joke: str):
-    completion = client.chat.completions.create(
+    completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
@@ -36,7 +37,7 @@ def translate_joke_to_pirate(joke: str):
 
 @tool(name="history_jokes")
 def history_jokes_tool():
-    completion = client.chat.completions.create(
+    completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": "get some history jokes"}],
     )
@@ -46,7 +47,7 @@ def history_jokes_tool():
 
 @task(name="signature_generation")
 def generate_signature(joke: str):
-    completion = client.completions.create(
+    completion = openai.Completion.create(
         model="text-davinci-003",
         prompt="add a signature to the joke:\n\n" + joke,
     )
