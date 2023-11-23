c0bd1697dfab0b7ed7b37d530d68d032ecc5a154
Gal Kleinman <gal.klm@gmail.com>
2023-11-23 22:12:09 +0200

    chore(llama-index): add missing newlines to resolve lint issues

--- packages/opentelemetry-instrumentation-llamaindex/opentelemetry/instrumentation/llamaindex/custom_llm_wrapper.py
+++ packages/opentelemetry-instrumentation-llamaindex/opentelemetry/instrumentation/llamaindex/custom_llm_wrapper.py
@@ -6,16 +6,19 @@ from opentelemetry.instrumentation.utils import _SUPPRESS_INSTRUMENTATION_KEY
 from opentelemetry.semconv.ai import SpanAttributes, TraceloopSpanKindValues, LLMRequestTypeValues
 from opentelemetry.instrumentation.llamaindex.utils import _with_tracer_wrapper
 
+
 def should_send_prompts():
     return (
         os.getenv("TRACELOOP_TRACE_CONTENT") or "true"
     ).lower() == "true" or context_api.get_value("override_enable_content_tracing")
+
 
 def _set_span_attribute(span, name, value):
     if value is not None:
         if value != "":
             span.set_attribute(name, value)
     return
+
 
 def _set_input_attributes(span, llm_request_type, args, kwargs, instance):
     _set_span_attribute(span, SpanAttributes.LLM_REQUEST_MODEL, instance.metadata.model_name)
@@ -37,12 +40,14 @@ def _set_input_attributes(span, llm_request_type, args, kwargs, instance):
 
     return
 
+
 def _set_response_attributes(span, llm_request_type, response):
     if should_send_prompts():
         if llm_request_type == LLMRequestTypeValues.COMPLETION:
             _set_span_attribute(span, f"{SpanAttributes.LLM_COMPLETIONS}.0.content", response.text)
 
     return
+
 
 @_with_tracer_wrapper
 def custom_llm_wrapper(tracer, to_wrap, wrapped, instance, args, kwargs):
