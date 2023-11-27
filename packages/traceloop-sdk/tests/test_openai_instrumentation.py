0f82bcc8c504b66adfd4dad80f8623a3bbc490b2
Nir Gazit <nirga@users.noreply.github.com>
2023-11-27 16:00:46 +0100

    fix(openai): attributes for functions in request (#211)

--- packages/traceloop-sdk/tests/test_openai_instrumentation.py
+++ packages/traceloop-sdk/tests/test_openai_instrumentation.py
@@ -138,6 +138,17 @@ def test_open_ai_function_calls(exporter, openai_client):
     spans = exporter.get_finished_spans()
     open_ai_span = spans[0]
     assert (
+        open_ai_span.attributes["llm.prompts.0.content"]
+        == "What's the weather like in Boston?"
+    )
+    assert (
+        open_ai_span.attributes["llm.request.functions.0.name"] == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes["llm.request.functions.0.description"]
+        == "Get the current weather in a given location"
+    )
+    assert (
         open_ai_span.attributes["llm.completions.0.function_call.name"]
         == "get_current_weather"
     )
