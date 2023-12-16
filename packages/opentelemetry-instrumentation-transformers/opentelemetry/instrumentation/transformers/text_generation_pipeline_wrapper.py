c3df3b120b20fc41f6d14dd7a24d655a46dc0cd0
Nir Gazit <nirga@users.noreply.github.com>
2023-12-16 22:12:03 +0100

    Make OpenLLMetry more robust to unexpected errors (#255)

--- packages/opentelemetry-instrumentation-transformers/opentelemetry/instrumentation/transformers/text_generation_pipeline_wrapper.py
+++ packages/opentelemetry-instrumentation-transformers/opentelemetry/instrumentation/transformers/text_generation_pipeline_wrapper.py
@@ -3,11 +3,9 @@ import logging
 from opentelemetry.semconv.ai import SpanAttributes
 from opentelemetry.trace import Status, StatusCode
 from opentelemetry import context as context_api
-from opentelemetry.instrumentation.utils import (
-    _SUPPRESS_INSTRUMENTATION_KEY
-)
+from opentelemetry.instrumentation.utils import _SUPPRESS_INSTRUMENTATION_KEY
 from opentelemetry.instrumentation.transformers.utils import _with_tracer_wrapper
-from transformers import TextGenerationPipeline
+import transformers
 
 logger = logging.getLogger(__name__)
 
@@ -39,13 +37,23 @@ def _set_input_attributes(span, instance, args, kwargs):
         prompts_list = kwargs.get("args")
 
     _set_span_prompts(span, prompts_list)
-    _set_span_attribute(span, SpanAttributes.LLM_REQUEST_MODEL, instance.model.config.name_or_path)
-    _set_span_attribute(span, SpanAttributes.LLM_VENDOR, instance.model.config.model_type)
+    _set_span_attribute(
+        span, SpanAttributes.LLM_REQUEST_MODEL, instance.model.config.name_or_path
+    )
+    _set_span_attribute(
+        span, SpanAttributes.LLM_VENDOR, instance.model.config.model_type
+    )
     _set_span_attribute(span, SpanAttributes.LLM_REQUEST_TYPE, "completion")
-    _set_span_attribute(span, SpanAttributes.LLM_TEMPERATURE, forward_params.get("temperature"))
+    _set_span_attribute(
+        span, SpanAttributes.LLM_TEMPERATURE, forward_params.get("temperature")
+    )
     _set_span_attribute(span, SpanAttributes.LLM_TOP_P, forward_params.get("top_p"))
-    _set_span_attribute(span, SpanAttributes.LLM_REQUEST_MAX_TOKENS, forward_params.get("max_length"))
-    _set_span_attribute(span, "llm.request.repetition_penalty", forward_params.get("repetition_penalty"))
+    _set_span_attribute(
+        span, SpanAttributes.LLM_REQUEST_MAX_TOKENS, forward_params.get("max_length")
+    )
+    _set_span_attribute(
+        span, "llm.request.repetition_penalty", forward_params.get("repetition_penalty")
+    )
 
     return
 
@@ -69,7 +77,9 @@ def _set_response_attributes(span, response):
 
 @_with_tracer_wrapper
 def text_generation_pipeline_wrapper(tracer, to_wrap, wrapped, instance, args, kwargs):
-    if not isinstance(instance, TextGenerationPipeline):
+    if "TextGenerationPipeline" not in dir(transformers) or not isinstance(
+        instance, transformers.TextGenerationPipeline
+    ):
         return wrapped(*args, **kwargs)
 
     if context_api.get_value(_SUPPRESS_INSTRUMENTATION_KEY):
@@ -83,7 +93,8 @@ def text_generation_pipeline_wrapper(tracer, to_wrap, wrapped, instance, args, k
 
             except Exception as ex:  # pylint: disable=broad-except
                 logger.warning(
-                    "Failed to set input attributes for transformers span, error: %s", str(ex)
+                    "Failed to set input attributes for transformers span, error: %s",
+                    str(ex),
                 )
 
         response = wrapped(*args, **kwargs)
