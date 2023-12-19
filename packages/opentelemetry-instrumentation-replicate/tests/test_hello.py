f070a288d55d672addffa49a870e1e1312e58fb3
Nir Gazit <nirga@users.noreply.github.com>
2023-12-19 16:53:29 +0100

    fix: broken tests (#281)

--- packages/opentelemetry-instrumentation-replicate/tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-"""Hello unit test module."""
-
-from opentelemetry.instrumentation.replicate.hello import hello
-
-
-def test_hello():
-    """Test the hello function."""
-    assert hello() == "Hello opentelemetry-instrumentation-replicate"
