4a115a55f986eef715e6464e00c20038bbd2d174
Kartik Prajapati <kartikprajapati987@gmail.com>
2023-12-19 17:01:09 +0530

    feat: replicate support (#248)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

---
+++ packages/sample-app/sample_app/replicate_functions.py
@@ -0,0 +1,21 @@
+import replicate
+
+from traceloop.sdk import Traceloop
+from traceloop.sdk.decorators import task, workflow
+
+Traceloop.init(app_name="image_generation_service")
+
+@task(name="image_generation")
+def generate_image():
+    images = replicate.run(
+      "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+      input={"prompt": "tiny robot"}
+    )
+    return images
+
+@workflow(name="robot_image_generator")
+def image_workflow():
+    print(generate_image())
+
+image_workflow()
+
