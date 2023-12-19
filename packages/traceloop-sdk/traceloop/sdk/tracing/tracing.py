711ccdc5410af4bf329c5a376222505c7b4de399
Kartik Prajapati <kartikprajapati987@gmail.com>
2023-12-19 17:01:09 +0530

    feat: replicate support (#248)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/traceloop-sdk/traceloop/sdk/tracing/tracing.py
+++ packages/traceloop-sdk/traceloop/sdk/tracing/tracing.py
@@ -264,8 +264,6 @@ def init_instrumentations():
     init_bedrock_instrumentor()
     init_replicate_instrumentor()
     init_vertexai_instrumentor()
-    init_replicate_instrumentor()
-    init_vertexai_instrumentor()
 
 
 def init_openai_instrumentor():
