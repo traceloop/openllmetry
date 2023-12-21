0c5af6b1f0e50c13011747a2197244777e4e1642
5war00p <maddisaiswaroop@gmail.com>
2023-12-21 13:22:44 +0530

    feat: integration of vertexai usage and tests

--- packages/traceloop-sdk/traceloop/sdk/tracing/tracing.py
+++ packages/traceloop-sdk/traceloop/sdk/tracing/tracing.py
@@ -391,7 +391,6 @@ def init_bedrock_instrumentor():
         if not instrumentor.is_instrumented_by_opentelemetry:
             instrumentor.instrument()
 
-
 def init_replicate_instrumentor():
     if importlib.util.find_spec("replicate") is not None:
         Telemetry().capture("instrumentation:replicate:init")
