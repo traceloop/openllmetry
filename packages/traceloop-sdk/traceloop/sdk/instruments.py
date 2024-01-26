25c7cd852fec85aeaf006cffd4e9476fd38ebaba
Mizba-Anjum <89572205+Mizba-Anjum@users.noreply.github.com>
2024-01-26 14:55:53 -0600

    Create manual instrumentation as well as auto instrumentation (#363)

---
+++ packages/traceloop-sdk/traceloop/sdk/instruments.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Instruments(Enum):
+    OPENAI = "openai"
+    ANTHROPIC = "anthropic"
+    COHERE = "cohere"
+    PINECONE = "pinecone"
+    CHROMA = "chroma"
+    LANGCHAIN = "langchain"
+    LLAMA_INDEX = "llama_index"
+    TRANSFORMERS = "transformers"
+    REQUESTS = "requests"
+    URLLIB3 = "urllib3"
+    PYMYSQL = "pymysql"
+    BEDROCK = "bedrock"
+    REPLICATE = "replicate"
+    VERTEXAI = "vertexai"
+    WATSONX = "watsonx"
