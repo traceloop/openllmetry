9754db87e02c8d0a2c37d232d988e854c7d32664
Alex Feel <71711753+alex-feel@users.noreply.github.com>
2024-02-20 00:05:00 +0200

    fix(llamaindex): support both new and legacy llama_index versions (#422)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-llamaindex/opentelemetry/instrumentation/llamaindex/base_retriever_instrumentor.py
+++ packages/opentelemetry-instrumentation-llamaindex/opentelemetry/instrumentation/llamaindex/base_retriever_instrumentor.py
@@ -1,9 +1,18 @@
+from importlib.metadata import version as package_version, PackageNotFoundError
+
 from wrapt import wrap_function_wrapper
 
-from opentelemetry.instrumentation.llamaindex.utils import _with_tracer_wrapper, start_as_current_span_async
+from opentelemetry.instrumentation.llamaindex.utils import (
+    _with_tracer_wrapper,
+    start_as_current_span_async,
+)
 from opentelemetry.semconv.ai import SpanAttributes, TraceloopSpanKindValues
 
-MODULE_NAME = "llama_index.indices.base_retriever"
+
+V9_MODULE_NAME = "llama_index.indices.base_retriever"
+V10_MODULE_NAME = "llama_index.core.indices.base_retriever"
+V10_LEGACY_MODULE_NAME = "llama_index.legacy.indices.base_retriever"
+
 CLASS_NAME = "BaseRetriever"
 TASK_NAME = "retrieve"
 
@@ -13,8 +22,21 @@ class BaseRetrieverInstrumentor:
         self._tracer = tracer
 
     def instrument(self):
-        wrap_function_wrapper(MODULE_NAME, f"{CLASS_NAME}.retrieve", retrieve_wrapper(self._tracer))
-        wrap_function_wrapper(MODULE_NAME, f"{CLASS_NAME}.aretrieve", aretrieve_wrapper(self._tracer))
+        try:
+            package_version("llama-index-core")
+            self._instrument_module(V10_MODULE_NAME)
+            self._instrument_module(V10_LEGACY_MODULE_NAME)
+
+        except PackageNotFoundError:
+            self._instrument_module(V9_MODULE_NAME)
+
+    def _instrument_module(self, module_name):
+        wrap_function_wrapper(
+            module_name, f"{CLASS_NAME}.retrieve", retrieve_wrapper(self._tracer)
+        )
+        wrap_function_wrapper(
+            module_name, f"{CLASS_NAME}.aretrieve", aretrieve_wrapper(self._tracer)
+        )
 
 
 @_with_tracer_wrapper
@@ -30,7 +52,9 @@ def retrieve_wrapper(tracer, wrapped, instance, args, kwargs):
 
 @_with_tracer_wrapper
 async def aretrieve_wrapper(tracer, wrapped, instance, args, kwargs):
-    async with start_as_current_span_async(tracer=tracer, name=f"{TASK_NAME}.task") as span:
+    async with start_as_current_span_async(
+        tracer=tracer, name=f"{TASK_NAME}.task"
+    ) as span:
         span.set_attribute(
             SpanAttributes.TRACELOOP_SPAN_KIND,
             TraceloopSpanKindValues.TASK.value,
--- packages/opentelemetry-instrumentation-llamaindex/opentelemetry/instrumentation/llamaindex/base_synthesizer_instrumentor.py
+++ packages/opentelemetry-instrumentation-llamaindex/opentelemetry/instrumentation/llamaindex/base_synthesizer_instrumentor.py
@@ -1,9 +1,17 @@
+from importlib.metadata import version as package_version, PackageNotFoundError
+
 from wrapt import wrap_function_wrapper
 
-from opentelemetry.instrumentation.llamaindex.utils import _with_tracer_wrapper, start_as_current_span_async
+from opentelemetry.instrumentation.llamaindex.utils import (
+    _with_tracer_wrapper,
+    start_as_current_span_async,
+)
 from opentelemetry.semconv.ai import SpanAttributes, TraceloopSpanKindValues
 
-MODULE_NAME = "llama_index.response_synthesizers"
+V9_MODULE_NAME = "llama_index.response_synthesizers"
+V10_MODULE_NAME = "llama_index.core.response_synthesizers"
+V10_LEGACY_MODULE_NAME = "llama_index.legacy.response_synthesizers.base"
+
 CLASS_NAME = "BaseSynthesizer"
 TASK_NAME = "synthesize"
 
@@ -13,8 +21,21 @@ class BaseSynthesizerInstrumentor:
         self._tracer = tracer
 
     def instrument(self):
-        wrap_function_wrapper(MODULE_NAME, f"{CLASS_NAME}.synthesize", synthesize_wrapper(self._tracer))
-        wrap_function_wrapper(MODULE_NAME, f"{CLASS_NAME}.asynthesize", asynthesize_wrapper(self._tracer))
+        try:
+            package_version("llama-index-core")
+            self._instrument_module(V10_MODULE_NAME)
+            self._instrument_module(V10_LEGACY_MODULE_NAME)
+
+        except PackageNotFoundError:
+            self._instrument_module(V9_MODULE_NAME)
+
+    def _instrument_module(self, module_name):
+        wrap_function_wrapper(
+            module_name, f"{CLASS_NAME}.synthesize", synthesize_wrapper(self._tracer)
+        )
+        wrap_function_wrapper(
+            module_name, f"{CLASS_NAME}.asynthesize", asynthesize_wrapper(self._tracer)
+        )
 
 
 @_with_tracer_wrapper
@@ -30,7 +51,9 @@ def synthesize_wrapper(tracer, wrapped, instance, args, kwargs):
 
 @_with_tracer_wrapper
 async def asynthesize_wrapper(tracer, wrapped, instance, args, kwargs):
-    async with start_as_current_span_async(tracer=tracer, name=f"{TASK_NAME}.task") as span:
+    async with start_as_current_span_async(
+        tracer=tracer, name=f"{TASK_NAME}.task"
+    ) as span:
         span.set_attribute(
             SpanAttributes.TRACELOOP_SPAN_KIND,
             TraceloopSpanKindValues.TASK.value,
