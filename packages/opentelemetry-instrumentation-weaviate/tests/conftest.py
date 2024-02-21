b9b0ece324533740debed24e37f6057001c5d7c3
Paolo Rechia <paolorechia@gmail.com>
2024-02-21 08:56:27 +0100

    Move test to weaviate own folder

--- packages/opentelemetry-instrumentation-weaviate/tests/conftest.py
+++ packages/opentelemetry-instrumentation-weaviate/tests/conftest.py
@@ -1,3 +1,44 @@
 """Unit tests configuration module."""
 
+import os
+import pytest
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+from opentelemetry.instrumentation.weaviate import WeaviateInstrumentor
+
 pytest_plugins = []
+
+
+@pytest.fixture(scope="session")
+def exporter():
+    exporter = InMemorySpanExporter()
+    processor = SimpleSpanProcessor(exporter)
+
+    provider = TracerProvider()
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
+
+    OpenAIInstrumentor().instrument()
+    WeaviateInstrumentor().instrument()
+
+    return exporter
+
+
+@pytest.fixture(autouse=True)
+def clear_exporter(exporter):
+    exporter.clear()
+
+
+@pytest.fixture(autouse=True)
+def environment():
+    os.environ["WEAVIATE_API_KEY"] = "weaviate-api-key"
+    os.environ["WEAVIATE_CLUSTER_URL"] = "weaviate-cluster-url"
+    os.environ["OPENAI_API_KEY"] = "open-api-key"
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {"filter_headers": ["authorization"]}
