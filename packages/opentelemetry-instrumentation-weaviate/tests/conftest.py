16ebdae1c8695f0b51c7fdebb2bec80857a34438
Paolo Rechia <paolorechia@gmail.com>
2024-02-27 14:01:54 +0100

    feat(weaviate): implement weaviate instrumentation (#394)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-weaviate/tests/conftest.py
+++ packages/opentelemetry-instrumentation-weaviate/tests/conftest.py
@@ -1,3 +1,48 @@
 """Unit tests configuration module."""
 
+import os
+import pytest
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+from opentelemetry.instrumentation.weaviate import WeaviateInstrumentor
+
 pytest_plugins = []
+
+
+@pytest.fixture(scope="session")
+def exporter():
+    exporter = InMemorySpanExporter()
+    processor = SimpleSpanProcessor(exporter)
+
+    provider = TracerProvider()
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
+
+    OpenAIInstrumentor().instrument()
+    WeaviateInstrumentor().instrument()
+
+    return exporter
+
+
+@pytest.fixture(autouse=True)
+def clear_exporter(exporter):
+    exporter.clear()
+
+
+@pytest.fixture(autouse=True)
+def environment():
+    os.environ["WEAVIATE_API_KEY"] = "api-key"
+    os.environ["WEAVIATE_CLUSTER_URL"] = (
+        "https://traceloop-sandbox-3azqlud3.weaviate.network"
+    )
+    os.environ["OPENAI_API_KEY"] = "open-api-key"
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": ["authorization", "x-openai-api-key"],
+    }
