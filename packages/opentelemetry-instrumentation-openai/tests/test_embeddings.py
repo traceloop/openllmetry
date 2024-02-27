8786970d2e904347f4762d87f34e33a169576cfd
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-02-27 18:31:13 +0900

    fix(openai): Fix `with_raw_response` redirect crashing span (#536)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-openai/tests/test_embeddings.py
+++ packages/opentelemetry-instrumentation-openai/tests/test_embeddings.py
@@ -24,6 +24,30 @@ def test_embeddings(exporter, openai_client):
 
 
 @pytest.mark.vcr
+def test_embeddings_with_raw_response(exporter, openai_client):
+    response = openai_client.embeddings.with_raw_response.create(
+        input="Tell me a joke about opentelemetry",
+        model="text-embedding-ada-002",
+    )
+    spans = exporter.get_finished_spans()
+    assert [span.name for span in spans] == [
+        "openai.embeddings",
+    ]
+    open_ai_span = spans[0]
+    assert (
+        open_ai_span.attributes["llm.prompts.0.content"]
+        == "Tell me a joke about opentelemetry"
+    )
+
+    assert open_ai_span.attributes["llm.request.model"] == "text-embedding-ada-002"
+    assert open_ai_span.attributes["llm.usage.prompt_tokens"] == 8
+    assert open_ai_span.attributes["openai.api_base"] == "https://api.openai.com/v1/"
+
+    parsed_response = response.parse()
+    assert parsed_response.data[0]
+
+
+@pytest.mark.vcr
 def test_azure_openai_embeddings(exporter):
     api_key = "redacted"
     azure_deployment = "redacted"
