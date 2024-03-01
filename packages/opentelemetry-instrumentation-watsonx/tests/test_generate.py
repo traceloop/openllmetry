da129c28cfdf846f79a71fc68d780994d96138c9
huang-cn <58250676+huang-cn@users.noreply.github.com>
2024-03-01 20:31:05 +0800

    fix(watsonx): Watsonx stream generate support (#552)

--- packages/opentelemetry-instrumentation-watsonx/tests/test_generate.py
+++ packages/opentelemetry-instrumentation-watsonx/tests/test_generate.py
@@ -15,3 +15,21 @@ def test_generate(exporter, watson_ai_model):
     assert watsonx_ai_span.attributes["llm.vendor"] == "Watsonx"
     assert watsonx_ai_span.attributes.get("llm.completions.content")
     assert watsonx_ai_span.attributes.get("llm.usage.total_tokens")
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="ibm-watson-ai requires python3.10")
+@pytest.mark.vcr
+def test_generate_text_stream(exporter, watson_ai_model):
+    if watson_ai_model is None:
+        print("test_generate_text_stream test skipped.")
+        return
+    response = watson_ai_model.generate_text_stream(prompt="Write an epigram about the sun")
+    generated_text = ""
+    for chunk in response:
+        generated_text += chunk
+    spans = exporter.get_finished_spans()
+    watsonx_ai_span = spans[1]
+    assert watsonx_ai_span.attributes["llm.prompts.user"] == "Write an epigram about the sun"
+    assert watsonx_ai_span.attributes["llm.vendor"] == "Watsonx"
+    assert watsonx_ai_span.attributes["llm.completions.content"] == generated_text
+    assert watsonx_ai_span.attributes.get("llm.usage.total_tokens")
