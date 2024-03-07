2538c2f7b263b4ea5286ce80318b4792c23d05fb
Nir Gazit <nirga@users.noreply.github.com>
2024-03-07 15:24:36 +0100

    fix(openai): is_streaming attribute (#589)

--- packages/opentelemetry-instrumentation-openai/tests/traces/test_chat.py
+++ packages/opentelemetry-instrumentation-openai/tests/traces/test_chat.py
@@ -2,6 +2,30 @@ import pytest
 
 
 @pytest.mark.vcr
+def test_chat(exporter, openai_client, vcr):
+    openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
+    )
+
+    spans = exporter.get_finished_spans()
+
+    assert [span.name for span in spans] == [
+        "openai.chat",
+    ]
+    open_ai_span = spans[0]
+    assert (
+        open_ai_span.attributes["llm.prompts.0.content"]
+        == "Tell me a joke about opentelemetry"
+    )
+    assert open_ai_span.attributes.get("llm.completions.0.content")
+    assert (
+        open_ai_span.attributes.get("openai.api_base") == "https://api.openai.com/v1/"
+    )
+    assert open_ai_span.attributes.get("llm.is_streaming") is False
+
+
+@pytest.mark.vcr
 def test_chat_streaming(exporter, openai_client, vcr):
     response = openai_client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -27,6 +51,7 @@ def test_chat_streaming(exporter, openai_client, vcr):
     assert (
         open_ai_span.attributes.get("openai.api_base") == "https://api.openai.com/v1/"
     )
+    assert open_ai_span.attributes.get("llm.is_streaming") is True
 
     events = open_ai_span.events
     assert len(events) == chunk_count
