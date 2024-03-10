d682e693dbf5748568e9e23381d46908f1b5fa9d
huang-cn <58250676+huang-cn@users.noreply.github.com>
2024-03-11 03:13:47 +0800

    feat(watsonx instrumentation): Watsonx metric support (#593)

---
+++ packages/opentelemetry-instrumentation-watsonx/tests/traces/conftest.py
@@ -0,0 +1,43 @@
+"""Unit tests configuration module."""
+
+import pytest
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.instrumentation.watsonx import WatsonxInstrumentor
+
+
+@pytest.fixture(scope="session")
+def exporter():
+    exporter = InMemorySpanExporter()
+    processor = SimpleSpanProcessor(exporter)
+
+    provider = TracerProvider()
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
+
+    try:
+        # Check for required package in the env, skip test if could not found
+        from ibm_watsonx_ai.foundation_models import ModelInference
+        # to avoid lint error
+        del ModelInference
+        WatsonxInstrumentor().instrument()
+    except ImportError:
+        print("no supported ibm_watsonx_ai package found, Watsonx instrumentation skipped.")
+
+    return exporter
+
+
+@pytest.fixture(autouse=True)
+def clear_exporter(exporter):
+    exporter.clear()
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": ["authorization"],
+        "allow_playback_repeats": True,
+        "decode_compressed_response": True,
+    }
