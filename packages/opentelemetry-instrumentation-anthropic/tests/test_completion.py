fa915d36bc2c1c0cf0d870c18aeae24e7d856ef2
Nir Gazit <nirga@users.noreply.github.com>
2024-03-21 23:41:40 +0200

    fix(anthropic): support messages API (#671)

--- packages/opentelemetry-instrumentation-anthropic/tests/test_completion.py
+++ packages/opentelemetry-instrumentation-anthropic/tests/test_completion.py
@@ -22,3 +22,32 @@ def test_anthropic_completion(exporter):
         == f"{HUMAN_PROMPT}\nHello world\n{AI_PROMPT}"
     )
     assert anthropic_span.attributes.get("llm.completions.0.content")
+
+
+@pytest.mark.vcr
+def test_anthropic_message_create(exporter):
+    client = Anthropic()
+    response = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Tell me a joke about OpenTelemetry",
+            }
+        ],
+        model="claude-3-opus-20240229",
+    )
+
+    spans = exporter.get_finished_spans()
+    assert [span.name for span in spans] == [
+        "anthropic.completion",
+    ]
+    anthropic_span = spans[0]
+    assert (
+        anthropic_span.attributes["llm.prompts.0.user"]
+        == "Tell me a joke about OpenTelemetry"
+    )
+    assert (
+        anthropic_span.attributes.get("llm.completions.0.content")
+        == response.content[0].text
+    )
