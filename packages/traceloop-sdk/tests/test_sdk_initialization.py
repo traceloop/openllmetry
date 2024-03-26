2b173f1310eea38dbe1333690ec69e9ee1207e69
Nir Gazit <nirga@users.noreply.github.com>
2024-03-26 11:55:48 +0200

    fix: test doesn't relay on other test mock data

--- packages/traceloop-sdk/tests/test_sdk_initialization.py
+++ packages/traceloop-sdk/tests/test_sdk_initialization.py
@@ -1,5 +1,6 @@
 import pytest
 from openai import OpenAI
+from traceloop.sdk.decorators import workflow
 
 
 @pytest.fixture
@@ -20,13 +21,13 @@ def test_resource_attributes(exporter, openai_client):
     assert open_ai_span.resource.attributes["service.name"] == "test"
 
 
-@pytest.mark.vcr("test_resource_attributes.yaml")
 def test_custom_span_processor(exporter_with_custom_span_processor, openai_client):
-    openai_client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
-    )
+    @workflow()
+    def run_workflow():
+        pass
+
+    run_workflow()
 
     spans = exporter_with_custom_span_processor.get_finished_spans()
-    open_ai_span = spans[0]
-    assert open_ai_span.attributes["custom_span"] == "yes"
+    workflow_span = spans[0]
+    assert workflow_span.attributes["custom_span"] == "yes"
