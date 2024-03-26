fe8ed0413d52f62f2e7cbe813ec4d526f980d155
EviSONG <evisong@live.com>
2024-03-26 16:39:07 +0800

    test(traceloop-sdk): UT for custom span processor

--- packages/traceloop-sdk/tests/test_sdk_initialization.py
+++ packages/traceloop-sdk/tests/test_sdk_initialization.py
@@ -18,3 +18,15 @@ def test_resource_attributes(exporter, openai_client):
     open_ai_span = spans[0]
     assert open_ai_span.resource.attributes["something"] == "yes"
     assert open_ai_span.resource.attributes["service.name"] == "test"
+
+
+@pytest.mark.vcr("test_resource_attributes.yaml")
+def test_custom_span_processor(exporter_with_custom_span_processor, openai_client):
+    openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "Tell me a joke about opentelemetry"}],
+    )
+
+    spans = exporter_with_custom_span_processor.get_finished_spans()
+    open_ai_span = spans[0]
+    assert open_ai_span.attributes["custom_span"] == "yes"
