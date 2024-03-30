f26fccd77367e79bc8bacae2d6d18134aa2abc15
midhun1998 <dev.mid.work@gmail.com>
2024-03-30 21:10:40 +0530

    Added context logic

--- packages/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/callbacks/span.py
+++ packages/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/callbacks/span.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, Any, Dict, Optional
 from langchain_core.callbacks.base import BaseCallbackHandler
 from langchain_core.utils import print_text
 from opentelemetry import context, propagators
+from opentelemetry.trace.span import Span
 from opentelemetry.semconv.ai import SpanAttributes, TraceloopSpanKindValues
 from opentelemetry.trace import Status, StatusCode, use_span, get_current_span, set_span_in_context
 
@@ -19,14 +20,13 @@ class SpanCallbackHandler(BaseCallbackHandler):
 
     def __init__(self, tracer: "Tracer", name: Optional[str] = None, kind: Optional[str] = None,  color: Optional[str] = None) -> None:
         """Initialize callback handler."""
-        super().__init__()
         self.color = color
         self._span_dict: Dict[str, "SPAN"] = {}
         self._tracer = tracer
         self._name = name
         self._kind = kind
 
-    def _create_span(self) -> None:
+    def _create_span(self, key) -> None:
         parent = get_current_span()
         span = self._tracer.start_span(self._name)
         span.set_attribute(
@@ -34,22 +34,29 @@ class SpanCallbackHandler(BaseCallbackHandler):
             self._kind,
         )
         span.set_attribute(SpanAttributes.TRACELOOP_ENTITY_NAME, self._name)
-        self._span_dict[self._name] = span
+        self._span_dict[key] = span
+        from opentelemetry import context as context_api
+        from opentelemetry.trace.propagation import _SPAN_KEY
+        context_api.attach(context_api.set_value(_SPAN_KEY, span))
+        if self._kind in TraceloopSpanKindValues.WORKFLOW.value:
+           context_api.attach(context_api.set_value("workflow_name", self._name))
+        # print(token)
         
-        if parent.is_recording():
-          set_span_in_context(span)
-        activation = use_span(span, end_on_exit=True)
-        activation.__enter__()
-    def _error_span(self, error) -> None:
-      current_span = self._span_dict.get(self._name, None)
-      if current_span:
-          current_span.set_status(Status(StatusCode.ERROR))
-          current_span.record_exception(error)
-          current_span.end()
-    def _end_span(self) -> None:
-      current_span = self._span_dict.get(self._name, None)
-      if current_span:
-        current_span.end()
+        # if parent.is_recording():
+        #   set_span_in_context(span)
+        activation = use_span(span)
+        # activation.__enter__()
+    def _error_span(self, error, key) -> None:
+      span = self._span_dict.get(key, None)
+      if isinstance(span, Span)  and span.is_recording():
+          span.set_status(Status(StatusCode.ERROR))
+          span.record_exception(error)
+          span.end()
+    def _end_span(self, key) -> None:
+      span = self._span_dict.get(key, None)
+      if isinstance(span, Span)  and span.is_recording():
+        print('ending')
+        span.end()
     def on_chain_start(
         self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
     ) -> None:
@@ -63,7 +70,7 @@ class SpanCallbackHandler(BaseCallbackHandler):
             self._kind = TraceloopSpanKindValues.WORKFLOW.value
           else:
             self._kind = TraceloopSpanKindValues.TASK.value
-        self._create_span()
+        self._create_span(kwargs.get('run_id', self._name))
         
         print(f"\n\n\033[1m> Entering new {self._name} chain...\033[0m")  # noqa: T201
         
@@ -72,12 +79,12 @@ class SpanCallbackHandler(BaseCallbackHandler):
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> Any:
         """Run when chain errors."""
-        self._error_span(error)        
+        self._error_span(error, kwargs.get('run_id', self._name))        
         print("\n\033[1m> Errored out chain.\033[0m")  # noqa: T201
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
         """Print out that we finished a chain."""
-        self._end_span()
+        self._end_span(kwargs.get('run_id', self._name))
         print("\n\033[1m> Finished chain.\033[0m")  # noqa: T201
 
     def on_agent_action(
