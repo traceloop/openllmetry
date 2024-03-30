11de42032471a46376ecac59df1ef16564bbccd5
Mayank Agarwal <mayankagarwal44442@gmail.com>
2024-03-30 23:43:02 +0530

    Fix tests

--- packages/opentelemetry-instrumentation-haystack/tests/test_simple_pipeline.py
+++ packages/opentelemetry-instrumentation-haystack/tests/test_simple_pipeline.py
@@ -1,9 +1,10 @@
 import os
 import pytest
 from haystack import Pipeline
-from haystack.components.generators import OpenAIChatGenerator
+from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.components.builders import DynamicChatPromptBuilder
 from haystack.dataclasses import ChatMessage
+from haystack.utils import Secret
 
 
 @pytest.mark.vcr
@@ -11,19 +12,16 @@ def test_haystack(exporter):
 
     prompt_builder = DynamicChatPromptBuilder()
     api_key=os.getenv("OPENAI_API_KEY"),
-    llm = OpenAIChatGenerator(api_key=api_key, model="gpt-4")
+    llm = OpenAIChatGenerator(api_key=Secret.from_token(api_key), model="gpt-4")
 
 
     pipe = Pipeline()
     pipe.add_component("prompt_builder", prompt_builder)
     pipe.add_component("llm", llm)
-
     pipe.connect("prompt_builder.prompt", "llm.messages")
-    location = "Berlin"
-    messages = [ChatMessage.from_system("Always respond in German even if some input data is in other languages."),
-                ChatMessage.from_user("Tell me about {{location}}")]
-    pipe.run(data={"prompt_builder": {"template_variables":{"location": location}, "prompt_source": messages}})
-
+    query = "OpenTelemetry"
+    messages = [ChatMessage.from_user("Tell me a joke about {{query}}")]
+    pipe.run(data={"prompt_builder": {"template_variables":{"query": query}, "prompt_source": messages}})
 
     spans = exporter.get_finished_spans()
     assert set(
