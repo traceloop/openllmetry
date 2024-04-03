533041f990f224ca350ff21ca36ef62f525e02d4
Nir Gazit <nirga@users.noreply.github.com>
2024-04-03 20:09:20 +0200

    fix(anthropic): support multi-modal (#746)

--- packages/opentelemetry-instrumentation-anthropic/tests/test_completion.py
+++ packages/opentelemetry-instrumentation-anthropic/tests/test_completion.py
@@ -1,4 +1,6 @@
+import json
 import pytest
+from pathlib import Path
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
 
@@ -46,6 +48,57 @@ def test_anthropic_message_create(exporter):
     assert (
         anthropic_span.attributes["llm.prompts.0.user"]
         == "Tell me a joke about OpenTelemetry"
+    )
+    assert (
+        anthropic_span.attributes.get("llm.completions.0.content")
+        == response.content[0].text
+    )
+
+
+@pytest.mark.vcr
+def test_anthropic_multi_modal(exporter):
+    client = Anthropic()
+    response = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What do you see?",
+                    },
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": "image/jpeg",
+                            "data": Path(__file__).parent.joinpath("data/logo.jpg"),
+                        },
+                    },
+                ],
+            },
+        ],
+        model="claude-3-opus-20240229",
+    )
+
+    spans = exporter.get_finished_spans()
+    assert [span.name for span in spans] == [
+        "anthropic.completion",
+    ]
+    anthropic_span = spans[0]
+    assert anthropic_span.attributes["llm.prompts.0.user"] == json.dumps(
+        [
+            {"type": "text", "text": "What do you see?"},
+            {
+                "type": "image",
+                "source": {
+                    "type": "base64",
+                    "media_type": "image/jpeg",
+                    "data": str(Path(__file__).parent.joinpath("data/logo.jpg")),
+                },
+            },
+        ]
     )
     assert (
         anthropic_span.attributes.get("llm.completions.0.content")
