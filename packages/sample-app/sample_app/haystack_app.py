6a42f95004c7e57055a7a14792330051f556ec2f
Mayank Agarwal <mayankagarwal44442@gmail.com>
2024-04-12 19:42:36 +0530

    Add complex pipeline in sample app:

--- packages/sample-app/sample_app/haystack_app.py
+++ packages/sample-app/sample_app/haystack_app.py
@@ -1,28 +1,67 @@
 import os
 from haystack import Pipeline
 from haystack.components.generators.chat import OpenAIChatGenerator
-from haystack.components.builders import DynamicChatPromptBuilder
+from haystack.components.builders import PromptBuilder
 from haystack.dataclasses import ChatMessage
 from haystack.utils import Secret
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from datasets import load_dataset
+from haystack import Document
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
 
 from traceloop.sdk import Traceloop
 
-Traceloop.init(app_name="haystack_example")
+Traceloop.init(app_name="haystack_example", disable_batch=True)
 
 def haystack_app():
     
-    prompt_builder = DynamicChatPromptBuilder()
+    # initialize
     api_key=os.getenv("OPENAI_API_KEY")
-    llm = OpenAIChatGenerator(api_key=Secret.from_token(api_key), model="gpt-4")
+    generator = OpenAIChatGenerator(api_key=Secret.from_token(api_key), model="gpt-3.5-turbo")
 
+    # initialize document store, load data and store in document store
+    document_store = InMemoryDocumentStore()
+    dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
+    docs = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
 
-    pipe = Pipeline()
-    pipe.add_component("prompt_builder", prompt_builder)
-    pipe.add_component("llm", llm)
-    pipe.connect("prompt_builder.prompt", "llm.messages")
-    query = "OpenTelemetry"
-    messages = [ChatMessage.from_user("Tell me a joke about {{query}}")]
-    pipe.run(data={"prompt_builder": {"template_variables":{"query": query}, "prompt_source": messages}})
+    doc_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+    doc_embedder.warm_up()
+    docs_with_embeddings = doc_embedder.run(docs)
+    document_store.write_documents(docs_with_embeddings["documents"])
 
+    # embedder to embed user query
+    text_embedder = SentenceTransformersTextEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+
+    # get relevant documents from embedded query
+    retriever = InMemoryEmbeddingRetriever(document_store)
+
+    # use documents to build the prompt
+    template = """
+    Given the following information, answer the question.
+
+    Context:
+    {% for document in documents %}
+        {{ document.content }}
+    {% endfor %}
+
+    Question: {{question}}
+    Answer:
+    """
+
+    prompt_builder = PromptBuilder(template=template)
+
+    basic_rag_pipeline = Pipeline()
+    # Add components to your pipeline
+    basic_rag_pipeline.add_component("text_embedder", text_embedder)
+    basic_rag_pipeline.add_component("retriever", retriever)
+    basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
+    basic_rag_pipeline.add_component("llm", generator)
+
+    # Now, connect the components to each other
+    basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+    basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
+    basic_rag_pipeline.connect("prompt_builder", "llm")
 
 haystack_app()
