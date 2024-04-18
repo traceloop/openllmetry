422985569991dffaab206e0f2ca6d2950bc8562c
Mayank Agarwal <mayankagarwal44442@gmail.com>
2024-04-18 23:48:55 +0530

    fix(haystack): V2 Support (#710)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

---
+++ packages/opentelemetry-instrumentation-haystack/tests/test_simple_pipeline.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+from haystack import Pipeline
+from haystack.components.generators.chat import OpenAIChatGenerator
+from haystack.components.builders import DynamicChatPromptBuilder
+from haystack.dataclasses import ChatMessage
+from haystack.utils import Secret
+
+
+@pytest.mark.vcr
+def test_haystack(exporter):
+
+    prompt_builder = DynamicChatPromptBuilder()
+    api_key = os.getenv("OPENAI_API_KEY")
+    llm = OpenAIChatGenerator(api_key=Secret.from_token(api_key), model="gpt-4")
+
+    pipe = Pipeline()
+    pipe.add_component("prompt_builder", prompt_builder)
+    pipe.add_component("llm", llm)
+    pipe.connect("prompt_builder.prompt", "llm.messages")
+    query = "OpenTelemetry"
+    messages = [ChatMessage.from_user("Tell me a joke about {{query}}")]
+    pipe.run(
+        data={
+            "prompt_builder": {
+                "template_variables": {"query": query},
+                "prompt_source": messages,
+            }
+        }
+    )
+
+    spans = exporter.get_finished_spans()
+    assert set(
+        [
+            "openai.chat",
+            "haystack_pipeline.workflow",
+        ]
+    ).issubset([span.name for span in spans])
