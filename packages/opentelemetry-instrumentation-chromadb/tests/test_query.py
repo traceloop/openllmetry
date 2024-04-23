43232f87755ad849c038cb0f057cbcdcd7a0c60c
Nir Gazit <nirga@users.noreply.github.com>
2024-04-23 16:27:35 +0200

    chore: tests

--- packages/opentelemetry-instrumentation-chromadb/tests/test_query.py
+++ packages/opentelemetry-instrumentation-chromadb/tests/test_query.py
@@ -12,7 +12,7 @@ def collection():
     chroma.delete_collection(name="Students")
 
 
-def add_documents(collection):
+def add_documents(collection, with_metadata=False):
     student_info = """
     Alexandra Thompson, a 19-year-old computer science sophomore with a 3.7 GPA,
     is a member of the programming and chess clubs who enjoys pizza, swimming, and hiking
@@ -33,19 +33,25 @@ def add_documents(collection):
     UW encompasses over 500 buildings and 20 million square feet of space,
     including one of the largest library systems in the world."""
 
-    collection.add(
-        documents=[student_info, club_info, university_info],
-        metadatas=[
-            {"source": "student info"},
-            {"source": "club info"},
-            {"source": "university info"},
-        ],
-        ids=["id1", "id2", "id3"],
-    )
+    if with_metadata:
+        collection.add(
+            documents=[student_info, club_info, university_info],
+            metadatas=[
+                {"source": "student info"},
+                {"source": "club info"},
+                {"source": "university info"},
+            ],
+            ids=["id1", "id2", "id3"],
+        )
+    else:
+        collection.add(
+            documents=[student_info, club_info, university_info],
+            ids=["id1", "id2", "id3"],
+        )
 
 
 def test_chroma_add(exporter, collection):
-    add_documents(collection)
+    add_documents(collection, with_metadata=True)
 
     spans = exporter.get_finished_spans()
     span = next(span for span in spans if span.name == "chroma.add")
@@ -57,17 +63,45 @@ def test_chroma_add(exporter, collection):
     assert span.attributes.get("db.chroma.add.documents_count") == 3
 
 
-def query_collection(collection):
+def test_chroma_query(exporter, collection):
+    add_documents(collection)
+    collection.query(
+        query_texts=["What is the student name?"],
+        n_results=2,
+    )
+
+    spans = exporter.get_finished_spans()
+    span = next(span for span in spans if span.name == "chroma.query")
+
+    assert span.attributes.get("db.system") == "chroma"
+    assert span.attributes.get("db.operation") == "query"
+    assert span.attributes.get("db.chroma.query.query_texts_count") == 1
+    assert span.attributes.get("db.chroma.query.n_results") == 2
+
+    events = span.events
+    assert len(events) == 1
+    for event in events:
+        assert event.name == "db.query.result"
+        ids_ = event.attributes.get(f"{event.name}.id")
+        distance = event.attributes.get(f"{event.name}.distance")
+        document = event.attributes.get(f"{event.name}.document")
+
+        assert len(ids_) > 0
+        assert isinstance(ids_, str)
+
+        assert distance >= 0
+
+        assert len(document) > 0
+        assert isinstance(document, str)
+
+
+def test_chroma_query_with_metadata(exporter, collection):
+    add_documents(collection, with_metadata=True)
     collection.query(
         query_texts=["What is the student name?"],
         n_results=2,
         where={"source": "student info"},
     )
-
-
-def test_chroma_query(exporter, collection):
-    add_documents(collection)
-    query_collection(collection)
 
     spans = exporter.get_finished_spans()
     span = next(span for span in spans if span.name == "chroma.query")
@@ -96,8 +130,11 @@ def test_chroma_query(exporter, collection):
 
 
 def test_chroma_query_segment_query(exporter, collection):
-    add_documents(collection)
-    query_collection(collection)
+    add_documents(collection, with_metadata=True)
+    collection.query(
+        query_texts=["What is the student name?"],
+        n_results=2,
+    )
 
     spans = exporter.get_finished_spans()
     span = next(span for span in spans if span.name == "chroma.query.segment._query")
