08232d09b3ab74793b7673bfc505aa39bf832ffc
Jose Cespedes <josecespedes@ibm.com>
2024-05-19 16:06:15 -0600

    Fixing test cases and augmenting test suite with local db

--- packages/opentelemetry-instrumentation-milvus/tests/test_query.py
+++ packages/opentelemetry-instrumentation-milvus/tests/test_query.py
@@ -1,8 +1,10 @@
 import pytest
 import pymilvus
 import random
+import os
 
-milvus = pymilvus.MilvusClient(uri="http://localhost:19530")
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "milvus.db")
+milvus = pymilvus.MilvusClient(uri=path)
 
 
 @pytest.fixture
@@ -10,11 +12,10 @@ def collection():
     collection_name = "Colors"
     milvus.create_collection(collection_name=collection_name, dimension=5)
     yield collection_name
-    milvus.release_collection(collection_name=collection_name)
     milvus.drop_collection(collection_name=collection_name)
 
 
-def insert_data(collection, create_partition=False):
+def insert_data(collection):
     colors = ["green", "blue", "yellow", "red", "black", "white", "purple", "pink", "orange", "grey"]
     data = [{
         "id": i,
@@ -46,27 +47,6 @@ def insert_data(collection, create_partition=False):
         i["color_tag"] = "{}_{}".format(i["color"], i["tag"])
     milvus.insert(collection_name=collection, data=data)
 
-    if create_partition:
-        milvus.create_partition(collection_name=collection, partition_name="partitionA")
-        part_data = [
-            {
-                "id": 1003,
-                "vector": [random.uniform(-1, 1) for _ in range(5)],
-                "color": "crimson",
-                "tag": 3489
-            },
-            {
-                "id": 1004,
-                "vector": [random.uniform(-1, 1) for _ in range(5)],
-                "color": "crimson",
-                "tag": 6453
-            }
-        ]
-        milvus.insert(collection_name=collection,
-                      data=part_data,
-                      partition_name="partitionA"
-                      )
-
 
 def test_milvus_insert(exporter, collection):
     insert_data(collection)
@@ -80,7 +60,7 @@ def test_milvus_insert(exporter, collection):
     assert span.attributes.get("db.milvus.insert.data_count") == 1003
 
 
-def test_milvus_query(exporter, collection):
+def test_milvus_query_equal(exporter, collection):
     insert_data(collection)
     milvus.query(
         collection_name=collection,
@@ -107,13 +87,12 @@ def test_milvus_query(exporter, collection):
         assert isinstance(_id, int)
 
 
-def test_milvus_query_partition(exporter, collection):
-    insert_data(collection, create_partition=True)
+def test_milvus_query_like(exporter, collection):
+    insert_data(collection)
     milvus.query(
         collection_name=collection,
-        filter='color == "crimson"',
+        filter='color_tag like "brown"',
         output_fields=["color_tag"],
-        partition_names=["partitionA"],
         limit=2
     )
     spans = exporter.get_finished_spans()
@@ -122,10 +101,61 @@ def test_milvus_query_partition(exporter, collection):
     assert span.attributes.get("db.system") == "milvus"
     assert span.attributes.get("db.operation") == "query"
     assert span.attributes.get("db.milvus.query.collection_name") == collection
-    assert span.attributes.get("db.milvus.query.filter") == 'color == "crimson"'
+    assert span.attributes.get("db.milvus.query.filter") == 'color_tag like "brown"'
     assert span.attributes.get("db.milvus.query.output_fields_count") == 1
     assert span.attributes.get("db.milvus.query.limit") == 2
-    assert span.attributes.get("db.milvus.query.partition_names_count") == 1
+    events = span.events
+    for event in events:
+        assert event.name == "db.query.result"
+        tag = event.attributes.get("color_tag")
+        _id = event.attributes.get("id")
+        assert isinstance(tag, str)
+        assert isinstance(_id, int)
+
+
+def test_milvus_query_or(exporter, collection):
+    insert_data(collection)
+    milvus.query(
+        collection_name=collection,
+        filter='color == "brown" or color == "red"',
+        output_fields=["color_tag"],
+        limit=3
+    )
+    spans = exporter.get_finished_spans()
+    span = next(span for span in spans if span.name == "milvus.query")
+
+    assert span.attributes.get("db.system") == "milvus"
+    assert span.attributes.get("db.operation") == "query"
+    assert span.attributes.get("db.milvus.query.collection_name") == collection
+    assert span.attributes.get("db.milvus.query.filter") == 'color == "brown" or color == "red"'
+    assert span.attributes.get("db.milvus.query.output_fields_count") == 1
+    assert span.attributes.get("db.milvus.query.limit") == 3
+    events = span.events
+    for event in events:
+        assert event.name == "db.query.result"
+        tag = event.attributes.get("color_tag")
+        _id = event.attributes.get("id")
+        assert isinstance(tag, str)
+        assert isinstance(_id, int)
+
+
+def test_milvus_query_and(exporter, collection):
+    insert_data(collection)
+    milvus.query(
+        collection_name=collection,
+        filter='color == "brown" and tag == 1234',
+        output_fields=["color_tag"],
+        limit=1
+    )
+    spans = exporter.get_finished_spans()
+    span = next(span for span in spans if span.name == "milvus.query")
+
+    assert span.attributes.get("db.system") == "milvus"
+    assert span.attributes.get("db.operation") == "query"
+    assert span.attributes.get("db.milvus.query.collection_name") == collection
+    assert span.attributes.get("db.milvus.query.filter") == 'color == "brown" and tag == 1234'
+    assert span.attributes.get("db.milvus.query.output_fields_count") == 1
+    assert span.attributes.get("db.milvus.query.limit") == 1
     events = span.events
     for event in events:
         assert event.name == "db.query.result"
