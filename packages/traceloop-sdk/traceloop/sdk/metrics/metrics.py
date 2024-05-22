9679f2006f24b59a7a824b4ae898e06b58d9127c
Gal Kleinman <gal.klm@gmail.com>
2024-05-22 17:50:11 +0300

    fix: add explicit buckets to pinecone histograms

--- packages/traceloop-sdk/traceloop/sdk/metrics/metrics.py
+++ packages/traceloop-sdk/traceloop/sdk/metrics/metrics.py
@@ -83,5 +83,17 @@ def metric_views() -> Sequence[View]:
             aggregation=ExplicitBucketHistogramAggregation(
                 [1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864]
             ),
-        )
+        ),
+        View(
+            instrument_name="db.pinecone.query.duration",
+            aggregation=ExplicitBucketHistogramAggregation(
+                [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28, 2.56, 5.12, 10.24, 20.48, 40.96, 81.92]
+            ),
+        ),
+        View(
+            instrument_name="db.pinecone.query.scores",
+            aggregation=ExplicitBucketHistogramAggregation(
+                [-1, -0.875, -0.75, -0.625, -0.5, -0.375, -0.25, -0.125, 0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+            ),
+        ),
     ]
