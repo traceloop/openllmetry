0694c523cda09a3a4463a2c0faa102ace5d21b73
Gal Kleinman <gal.klm@gmail.com>
2024-05-22 16:25:47 +0300

    lint

--- packages/traceloop-sdk/traceloop/sdk/metrics/metrics.py
+++ packages/traceloop-sdk/traceloop/sdk/metrics/metrics.py
@@ -75,7 +75,7 @@ def metric_views() -> Sequence[View]:
         View(
             instrument_name="gen_ai.client.operation.duration",
             aggregation=ExplicitBucketHistogramAggregation(
-                [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28, 2.56, 5.12,10.24, 20.48, 40.96, 81.92]
+                [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28, 2.56, 5.12, 10.24, 20.48, 40.96, 81.92]
             ),
         ),
         View(
