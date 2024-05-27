803884108cc14cdf82359346334bf877594573c2
Tibor Reiss <tibor.reiss@gmail.com>
2024-05-27 18:56:36 +0200

    Input from code review

--- packages/sample-app/sample_app/weaviate_v3.py
+++ packages/sample-app/sample_app/weaviate_v3.py
@@ -1,5 +1,4 @@
 # Tested with weaviate-client==3.26.0
-# Weaviate instrumentation with opentelemetry-instrumentation-weaviate==0.19.0
 # Code is adapted from official documentation.
 # V3 documentation: https://weaviate.io/developers/weaviate/client-libraries/python/python_v3
 # Some parts were also adapted from:
