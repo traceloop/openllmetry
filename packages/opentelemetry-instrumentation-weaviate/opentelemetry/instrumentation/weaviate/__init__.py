659efb312e1ef4019c6d2215a5e86e0be946885c
Tibor Reiss <75096465+tibor-reiss@users.noreply.github.com>
2024-05-28 08:52:51 +0200

    fix(weaviate): support v4.6.3 (#1134)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-weaviate/opentelemetry/instrumentation/weaviate/__init__.py
+++ packages/opentelemetry-instrumentation-weaviate/opentelemetry/instrumentation/weaviate/__init__.py
@@ -15,10 +15,10 @@ from wrapt import wrap_function_wrapper
 
 logger = logging.getLogger(__name__)
 
-_instruments = ("weaviate-client >= 3.26.0, <4",)
+_instruments = ("weaviate-client >= 3.26.0, <5",)
 
 
-WRAPPED_METHODS = [
+WRAPPED_METHODS_v3 = [
     {
         "module": "weaviate.schema",
         "object": "Schema",
@@ -109,13 +109,106 @@ WRAPPED_METHODS = [
         "method": "do",
         "span_name": "db.weaviate.gql.query.get.do",
     },
+]
+WRAPPED_METHODS_v4 = [
+    {
+        "module": "weaviate.collections.collections",
+        "object": "_Collections",
+        "method": "get",
+        "span_name": "db.weaviate.collections.get",
+    },
+    {
+        "module": "weaviate.collections.collections",
+        "object": "_Collections",
+        "method": "create",
+        "span_name": "db.weaviate.collections.create",
+    },
+    {
+        "module": "weaviate.collections.collections",
+        "object": "_Collections",
+        "method": "create_from_dict",
+        "span_name": "db.weaviate.collections.create_from_dict",
+    },
+    {
+        "module": "weaviate.collections.collections",
+        "object": "_Collections",
+        "method": "delete",
+        "span_name": "db.weaviate.collections.delete",
+    },
+    {
+        "module": "weaviate.collections.collections",
+        "object": "_Collections",
+        "method": "delete_all",
+        "span_name": "db.weaviate.collections.delete_all",
+    },
+    {
+        "module": "weaviate.collections.data",
+        "object": "_DataCollection",
+        "method": "insert",
+        "span_name": "db.weaviate.collections.data.insert",
+    },
+    {
+        "module": "weaviate.collections.data",
+        "object": "_DataCollection",
+        "method": "replace",
+        "span_name": "db.weaviate.collections.data.replace",
+    },
+    {
+        "module": "weaviate.collections.data",
+        "object": "_DataCollection",
+        "method": "update",
+        "span_name": "db.weaviate.collections.data.update",
+    },
+    {
+        "module": "weaviate.collections.batch.collection",
+        "object": "_BatchCollection",
+        "method": "add_object",
+        "span_name": "db.weaviate.collections.batch.add_object",
+    },
+    {
+        "module": "weaviate.collections.queries.fetch_object_by_id.query",
+        "object": "_FetchObjectByIDQuery",
+        "method": "fetch_object_by_id",
+        "span_name": "db.weaviate.collections.query.fetch_object_by_id",
+    },
+    {
+        "module": "weaviate.collections.queries.fetch_objects.query",
+        "object": "_FetchObjectsQuery",
+        "method": "fetch_objects",
+        "span_name": "db.weaviate.collections.query.fetch_objects",
+    },
+    {
+        "module": "weaviate.collections.grpc.query",
+        "object": "_QueryGRPC",
+        "method": "get",
+        "span_name": "db.weaviate.collections.query.get",
+    },
     {
         "module": "weaviate.gql.filter",
         "object": "GraphQL",
         "method": "do",
-        "span_name": "db.weaviate.gql.query.filter.do",
+        "span_name": "db.weaviate.gql.filter.do",
+    },
+    {
+        "module": "weaviate.gql.aggregate",
+        "object": "AggregateBuilder",
+        "method": "do",
+        "span_name": "db.weaviate.gql.aggregate.do",
+    },
+    {
+        "module": "weaviate.gql.get",
+        "object": "GetBuilder",
+        "method": "do",
+        "span_name": "db.weaviate.gql.get.do",
+    },
+    {
+        "module": "weaviate.client",
+        "object": "WeaviateClient",
+        "method": "graphql_raw_query",
+        "span_name": "db.weaviate.client.graphql_raw_query",
     },
 ]
+WRAPPED_METHODS = WRAPPED_METHODS_v3 + WRAPPED_METHODS_v4
 
 
 class WeaviateInstrumentor(BaseInstrumentor):
