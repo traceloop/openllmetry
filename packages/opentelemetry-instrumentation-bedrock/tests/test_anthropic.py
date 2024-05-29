bd73664ac6794cb4abb0f469f8586b294a189f9a
Nir Gazit <nirga@users.noreply.github.com>
2024-05-29 15:16:31 +0300

    fix(bedrock): support streaming API (#1179)

--- packages/opentelemetry-instrumentation-bedrock/tests/test_anthropic.py
+++ packages/opentelemetry-instrumentation-bedrock/tests/test_anthropic.py
@@ -94,6 +94,64 @@ def test_anthropic_3_completion_complex_content(exporter, brt):
 
 
 @pytest.mark.vcr
+def test_anthropic_3_completion_streaming(exporter, brt):
+    body = json.dumps(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "Tell me a joke about opentelemetry"},
+                    ],
+                }
+            ],
+            "max_tokens": 200,
+            "temperature": 0.5,
+            "anthropic_version": "bedrock-2023-05-31",
+        }
+    )
+
+    response = brt.invoke_model_with_response_stream(
+        body=body,
+        modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+        accept="application/json",
+        contentType="application/json",
+    )
+
+    completion = ""
+    for event in response.get("body"):
+        chunk = event.get("chunk")
+        if chunk:
+            decoded_chunk = json.loads(chunk.get("bytes").decode())
+            if "delta" in decoded_chunk:
+                completion += decoded_chunk.get("delta").get("text") or ""
+
+    spans = exporter.get_finished_spans()
+    assert all(span.name == "bedrock.completion" for span in spans)
+
+    anthropic_span = spans[0]
+    assert json.loads(anthropic_span.attributes["gen_ai.prompt.0.content"]) == [
+        {"type": "text", "text": "Tell me a joke about opentelemetry"},
+    ]
+
+    assert json.loads(anthropic_span.attributes.get("gen_ai.completion.0.content")) == [
+        {
+            "type": "text",
+            "text": completion,
+        }
+    ]
+
+    assert anthropic_span.attributes.get("gen_ai.usage.prompt_tokens") == 9
+    assert anthropic_span.attributes.get(
+        "gen_ai.usage.completion_tokens"
+    ) + anthropic_span.attributes.get(
+        "gen_ai.usage.prompt_tokens"
+    ) == anthropic_span.attributes.get(
+        "llm.usage.total_tokens"
+    )
+
+
+@pytest.mark.vcr
 def test_anthropic_3_completion_string_content(exporter, brt):
     body = json.dumps(
         {
