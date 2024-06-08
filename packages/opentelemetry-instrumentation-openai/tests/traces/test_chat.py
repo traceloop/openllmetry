6b9b815cb9218958c93ad476e216039848f5b7f3
achang97 <aachang97@gmail.com>
2024-06-08 11:59:09 -0700

    feat(instrumentation): add streamed OpenAI function tracing (#1284)

--- packages/opentelemetry-instrumentation-openai/tests/traces/test_chat.py
+++ packages/opentelemetry-instrumentation-openai/tests/traces/test_chat.py
@@ -2,6 +2,29 @@ import pytest
 from opentelemetry.semconv.ai import SpanAttributes
 
 
+@pytest.fixture
+def openai_tools():
+    return [
+        {
+            "type": "function",
+            "function": {
+                "name": "get_current_weather",
+                "description": "Get the current weather",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "location": {
+                            "type": "string",
+                            "description": "The city and state, e.g. San Francisco, CA",
+                        },
+                    },
+                    "required": ["location"],
+                },
+            },
+        },
+    ]
+
+
 @pytest.mark.vcr
 def test_chat(exporter, openai_client):
     openai_client.chat.completions.create(
@@ -31,6 +54,44 @@ def test_chat(exporter, openai_client):
         == "fp_2b778c6b35"
     )
     assert open_ai_span.attributes.get(SpanAttributes.LLM_IS_STREAMING) is False
+
+
+@pytest.mark.vcr
+def test_chat_tools(exporter, openai_client, openai_tools):
+    openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "What's the weather like in San Francisco?"}
+        ],
+        tools=openai_tools,
+    )
+
+    spans = exporter.get_finished_spans()
+
+    assert [span.name for span in spans] == [
+        "openai.chat",
+    ]
+    open_ai_span = spans[0]
+    assert (
+        open_ai_span.attributes.get(f"{SpanAttributes.LLM_REQUEST_FUNCTIONS}.0.name")
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes.get(f"{SpanAttributes.LLM_COMPLETIONS}.0.finish_reason")
+        == "tool_calls"
+    )
+    assert (
+        open_ai_span.attributes.get(
+            f"{SpanAttributes.LLM_COMPLETIONS}.0.function_call.name"
+        )
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes.get(
+            f"{SpanAttributes.LLM_COMPLETIONS}.0.function_call.arguments"
+        )
+        == '{"location":"San Francisco"}'
+    )
 
 
 @pytest.mark.vcr
@@ -76,6 +137,48 @@ def test_chat_streaming(exporter, openai_client):
 
 
 @pytest.mark.vcr
+def test_chat_tools_streaming(exporter, openai_client, openai_tools):
+    response = openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "What's the weather like in San Francisco?"}
+        ],
+        tools=openai_tools,
+        stream=True,
+    )
+
+    for _ in response:
+        pass
+
+    spans = exporter.get_finished_spans()
+
+    assert [span.name for span in spans] == [
+        "openai.chat",
+    ]
+    open_ai_span = spans[0]
+    assert (
+        open_ai_span.attributes.get(f"{SpanAttributes.LLM_REQUEST_FUNCTIONS}.0.name")
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes.get(f"{SpanAttributes.LLM_COMPLETIONS}.0.finish_reason")
+        == "tool_calls"
+    )
+    assert (
+        open_ai_span.attributes.get(
+            f"{SpanAttributes.LLM_COMPLETIONS}.0.function_call.name"
+        )
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes.get(
+            f"{SpanAttributes.LLM_COMPLETIONS}.0.function_call.arguments"
+        )
+        == '{"location":"San Francisco"}'
+    )
+
+
+@pytest.mark.vcr
 @pytest.mark.asyncio
 async def test_chat_async_streaming(exporter, async_openai_client):
     response = await async_openai_client.chat.completions.create(
@@ -116,3 +219,46 @@ async def test_chat_async_streaming(exporter, async_openai_client):
     total_tokens = open_ai_span.attributes.get(SpanAttributes.LLM_USAGE_TOTAL_TOKENS)
     assert completion_tokens and prompt_tokens and total_tokens
     assert completion_tokens + prompt_tokens == total_tokens
+
+
+@pytest.mark.vcr
+@pytest.mark.asyncio
+async def test_chat_tools_async_streaming(exporter, async_openai_client, openai_tools):
+    response = await async_openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "What's the weather like in San Francisco?"}
+        ],
+        tools=openai_tools,
+        stream=True,
+    )
+
+    async for _ in response:
+        pass
+
+    spans = exporter.get_finished_spans()
+
+    assert [span.name for span in spans] == [
+        "openai.chat",
+    ]
+    open_ai_span = spans[0]
+    assert (
+        open_ai_span.attributes.get(f"{SpanAttributes.LLM_REQUEST_FUNCTIONS}.0.name")
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes.get(f"{SpanAttributes.LLM_COMPLETIONS}.0.finish_reason")
+        == "tool_calls"
+    )
+    assert (
+        open_ai_span.attributes.get(
+            f"{SpanAttributes.LLM_COMPLETIONS}.0.function_call.name"
+        )
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes.get(
+            f"{SpanAttributes.LLM_COMPLETIONS}.0.function_call.arguments"
+        )
+        == '{"location":"San Francisco"}'
+    )
