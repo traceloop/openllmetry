d46d8260ccecf0e7c273e55299a0ec6ea624f1bd
Gal Kleinman <33281963+galkleinman@users.noreply.github.com>
2024-06-17 21:42:59 +0300

    fix: input/output serialization issue for langchain (#1341)

---
+++ packages/traceloop-sdk/tests/test_tasks.py
@@ -0,0 +1,28 @@
+import json
+import pytest
+
+from langchain_openai import ChatOpenAI
+from traceloop.sdk.decorators import task
+from opentelemetry.semconv.ai import SpanAttributes
+
+
+@pytest.mark.vcr
+def test_task_io_serialization_with_langchain(exporter):
+    @task(name="answer_question")
+    def answer_question():
+        chat = ChatOpenAI(temperature=0)
+
+        return chat.invoke("Is Berlin the capital of Germany? Answer with yes or no")
+
+    answer_question()
+
+    spans = exporter.get_finished_spans()
+
+    assert [span.name for span in spans] == [
+        'openai.chat',
+        'ChatOpenAI.langchain.task',
+        'answer_question.task',
+    ]
+
+    task_span = next(span for span in spans if span.name == 'answer_question.task')
+    assert (json.loads(task_span.attributes.get(SpanAttributes.TRACELOOP_ENTITY_OUTPUT))["kwargs"]["content"] == "Yes")
