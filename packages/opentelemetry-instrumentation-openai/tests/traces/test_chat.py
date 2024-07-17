9b4b455805aa07b6c20aae07f9bfa32b8335cfeb
Nir Gazit <nirga@users.noreply.github.com>
2024-07-17 11:30:56 +0200

    fix(openai): pydantic tool calls in prompt weren't serialized correctly

--- packages/opentelemetry-instrumentation-openai/tests/traces/test_chat.py
+++ packages/opentelemetry-instrumentation-openai/tests/traces/test_chat.py
@@ -1,6 +1,9 @@
-import json
 import pytest
 from opentelemetry.semconv.ai import SpanAttributes
+from openai.types.chat.chat_completion_message_tool_call import (
+    ChatCompletionMessageToolCall,
+    Function,
+)
 
 
 @pytest.mark.vcr
@@ -68,27 +71,71 @@ def test_chat_tool_calls(exporter, openai_client):
     open_ai_span = spans[0]
 
     assert f"{SpanAttributes.LLM_PROMPTS}.0.content" not in open_ai_span.attributes
-    assert open_ai_span.attributes[
-        f"{SpanAttributes.LLM_PROMPTS}.0.tool_calls"
-    ] == json.dumps(
-        [
-            {
-                "id": "1",
-                "type": "function",
-                "function": {
-                    "name": "get_current_weather",
-                    "arguments": '{"location": "San Francisco"}',
-                },
-            }
+    assert (
+        open_ai_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.0.tool_calls.0.name"]
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes[
+            f"{SpanAttributes.LLM_PROMPTS}.0.tool_calls.0.arguments"
         ]
+        == '{"location": "San Francisco"}'
     )
 
     assert (
         open_ai_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.content"]
         == "The weather in San Francisco is 70 degrees and sunny."
     )
+
+
+@pytest.mark.vcr
+def test_chat_pydantic_based_tool_calls(exporter, openai_client):
+    openai_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "assistant",
+                "tool_calls": [
+                    ChatCompletionMessageToolCall(
+                        id="1",
+                        type="function",
+                        function=Function(
+                            name="get_current_weather",
+                            arguments='{"location": "San Francisco"}',
+                        ),
+                    )
+                ],
+            },
+            {
+                "role": "tool",
+                "tool_call_id": "1",
+                "content": "The weather in San Francisco is 70 degrees and sunny.",
+            },
+        ],
+    )
+
+    spans = exporter.get_finished_spans()
+
+    assert [span.name for span in spans] == [
+        "openai.chat",
+    ]
+    open_ai_span = spans[0]
+
+    assert f"{SpanAttributes.LLM_PROMPTS}.0.content" not in open_ai_span.attributes
     assert (
-        open_ai_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.tool_call_id"] == "1"
+        open_ai_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.0.tool_calls.0.name"]
+        == "get_current_weather"
+    )
+    assert (
+        open_ai_span.attributes[
+            f"{SpanAttributes.LLM_PROMPTS}.0.tool_calls.0.arguments"
+        ]
+        == '{"location": "San Francisco"}'
+    )
+
+    assert (
+        open_ai_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.content"]
+        == "The weather in San Francisco is 70 degrees and sunny."
     )
 
 
