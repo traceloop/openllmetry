4597a26843e9c084da4adc6e521591b55696b159
Jinsong Wang <64242752+jinsongo@users.noreply.github.com>
2024-08-28 03:39:51 -0700

    Correct the package names for watsonx (#1902)

--- packages/traceloop-sdk/traceloop/sdk/tracing/tracing.py
+++ packages/traceloop-sdk/traceloop/sdk/tracing/tracing.py
@@ -928,7 +928,7 @@ def init_vertexai_instrumentor():
 
 def init_watsonx_instrumentor():
     try:
-        if is_package_installed("ibm_watson_machine_learning"):
+        if is_package_installed("ibm-watsonx-ai") or is_package_installed("ibm-watson-machine-learning"):
             Telemetry().capture("instrumentation:watsonx:init")
             from opentelemetry.instrumentation.watsonx import WatsonxInstrumentor
 
