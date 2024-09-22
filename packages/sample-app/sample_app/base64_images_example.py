82875dbb3679eb53b5cfd147100a7504a22ef026
Gal Kleinman <gal.klm@gmail.com>
2024-09-23 00:27:35 +0300

    fix sample app lint

--- packages/sample-app/sample_app/base64_images_example.py
+++ packages/sample-app/sample_app/base64_images_example.py
@@ -2,52 +2,40 @@ import base64
 import os
 import requests
 
+
 def upload_base64_image(image_path, message_index, message_content_index, image_format, trace_id, span_id, api_key):
-    # Read the image file and encode it to base64
     with open(image_path, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
 
-    # Prepare the payload
     payload = {
-        "message_index": message_index, 
+        "message_index": message_index,
         "message_content_index": message_content_index,
         "image_data": encoded_image,
         "image_format": image_format
     }
 
-    # Construct the URL
     url = f"http://localhost:3000/traces/{trace_id}/spans/{span_id}/images"
-
-    # Prepare the headers with the API key
     headers = {
         "Authorization": f"Bearer {api_key}"
     }
 
-
-    # Send POST request with headers
     response = requests.post(url, json=payload, headers=headers)
 
-    # Check the response
     if response.status_code == 200:
         print(f"Image {image_name} uploaded successfully.")
     else:
         print(f"Failed to upload image {image_name}. Status code: {response.status_code}")
 
-def get_image_urls(trace_id, span_id, api_key):
-    # Construct the URL
-    url = f"http://localhost:3000/traces/{trace_id}/spans/{span_id}/images"
 
-    # Prepare the headers with the API key
+def get_image_urls(trace_id, span_id, api_key):
+    url = f"http://localhost:3000/traces/{trace_id}/spans/{span_id}/images"
     headers = {
         "Authorization": f"Bearer {api_key}"
     }
-
-    # Send GET request with headers
     response = requests.get(url, headers=headers)
 
     print(response.json())
 
-    # Check the response
     if response.status_code == 200:
         image_url = response.json().get('url')
         return image_url
@@ -55,7 +43,7 @@ def get_image_urls(trace_id, span_id, api_key):
         print(f"Failed to retrieve image URL. Status code: {response.status_code}")
         return None
 
-# Example usage
+
 if __name__ == "__main__":
     image_path = "/Users/galklm/Downloads/itamar.jpeg"
     image_name = "itamar.jpeg"
--- packages/sample-app/sample_app/openai_vision_base64_example.py
+++ packages/sample-app/sample_app/openai_vision_base64_example.py
@@ -1,22 +1,21 @@
 import base64
 import os
 from openai import OpenAI
-import requests
 from traceloop.sdk import Traceloop
 from traceloop.sdk.decorators import workflow, task
 
-# OpenAI API Key
+
 api_key = os.environ.get("OPENAI_API_KEY")
 
-# Function to encode the image
+
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Path to your image
-image_path =  "/Users/galklm/Downloads/elephant.jpeg"
 
-# Getting the base64 string
+image_path = "/Users/galklm/Downloads/elephant.jpeg"
+
+
 base64_image = encode_image(image_path)
 
 headers = {
@@ -48,35 +47,38 @@ payload = {
 
 Traceloop.init()
 
+
 @task(name="generate_description")
 def task():
-  client = OpenAI(api_key=api_key)
-  completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-      {
-        "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": "What’s in this image?"
-          },
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": f"data:image/jpeg;base64,{base64_image}"
+    client = OpenAI(api_key=api_key)
+    completion = client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": "What’s in this image?"
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+              }
             }
-          }
-        ]
-      }
-    ],
-  )
+          ]
+        }
+      ],
+    )
 
-  print(completion)
+    print(completion)
+
 
 @workflow(name="image_description_generation")
 def workflow():
-  task()
+    task()
+
 
 if __name__ == "__main__":
-  workflow()
+    workflow()
