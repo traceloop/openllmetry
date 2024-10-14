fbe8ff99cb5d9454da61cc884141708aae273501
Ronen Schaffer <ronen.schaffer@ibm.com>
2024-10-14 17:16:24 +0300

    Add trace context to client requests

--- packages/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/__init__.py
+++ packages/opentelemetry-instrumentation-langchain/opentelemetry/instrumentation/langchain/__init__.py
@@ -12,6 +12,10 @@ from opentelemetry.instrumentation.utils import unwrap
 
 from opentelemetry.instrumentation.langchain.version import __version__
 
+from opentelemetry.trace.propagation.tracecontext import (
+    TraceContextTextMapPropagator,
+)
+from opentelemetry.trace.propagation import set_span_in_context
 
 from opentelemetry.instrumentation.langchain.callback_handler import (
     TraceloopCallbackHandler,
@@ -25,9 +29,10 @@ _instruments = ("langchain >= 0.0.346", "langchain-core > 0.1.0")
 class LangchainInstrumentor(BaseInstrumentor):
     """An instrumentor for Langchain SDK."""
 
-    def __init__(self, exception_logger=None):
+    def __init__(self, exception_logger=None, disable_trace_context_propagation=False):
         super().__init__()
         Config.exception_logger = exception_logger
+        self.disable_trace_context_propagation = disable_trace_context_propagation
 
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
@@ -36,14 +41,109 @@ class LangchainInstrumentor(BaseInstrumentor):
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(__name__, __version__, tracer_provider)
 
+        traceloopCallbackHandler = TraceloopCallbackHandler(tracer)
         wrap_function_wrapper(
             module="langchain_core.callbacks",
             name="BaseCallbackManager.__init__",
-            wrapper=_BaseCallbackManagerInitWrapper(TraceloopCallbackHandler(tracer)),
+            wrapper=_BaseCallbackManagerInitWrapper(traceloopCallbackHandler),
         )
+
+        if not self.disable_trace_context_propagation:
+            self._wrap_openai_functions_for_tracing(traceloopCallbackHandler)
+
+    def _wrap_openai_functions_for_tracing(self, traceloopCallbackHandler):
+        openai_tracing_wrapper = _OpenAITracingWrapper(traceloopCallbackHandler)
+
+        # Wrap langchain_community.llms.openai.BaseOpenAI
+        wrap_function_wrapper(
+            module="langchain_community.llms.openai",
+            name="BaseOpenAI._generate",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_community.llms.openai",
+            name="BaseOpenAI._agenerate",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_community.llms.openai",
+            name="BaseOpenAI._stream",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_community.llms.openai",
+            name="BaseOpenAI._astream",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        # Wrap langchain_openai.llms.base.BaseOpenAI
+        wrap_function_wrapper(
+            module="langchain_openai.llms.base",
+            name="BaseOpenAI._generate",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_openai.llms.base",
+            name="BaseOpenAI._agenerate",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_openai.llms.base",
+            name="BaseOpenAI._stream",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_openai.llms.base",
+            name="BaseOpenAI._astream",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        # langchain_openai.chat_models.base.BaseOpenAI
+        wrap_function_wrapper(
+            module="langchain_openai.chat_models.base",
+            name="BaseChatOpenAI._generate",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        wrap_function_wrapper(
+            module="langchain_openai.chat_models.base",
+            name="BaseChatOpenAI._agenerate",
+            wrapper=openai_tracing_wrapper,
+        )
+
+        # Doesn't work :(
+        # wrap_function_wrapper(
+        #     module="langchain_openai.chat_models.base",
+        #     name="BaseChatOpenAI._stream",
+        #     wrapper=openai_tracing_wrapper,
+        # )
+        # wrap_function_wrapper(
+        #     module="langchain_openai.chat_models.base",
+        #     name="BaseChatOpenAI._astream",
+        #     wrapper=openai_tracing_wrapper,
+        # )
 
     def _uninstrument(self, **kwargs):
         unwrap("langchain_core.callbacks", "BaseCallbackManager.__init__")
+        if not self.disable_trace_context_propagation:
+            unwrap("langchain_community.llms.openai", "BaseOpenAI._generate")
+            unwrap("langchain_community.llms.openai", "BaseOpenAI._agenerate")
+            unwrap("langchain_community.llms.openai", "BaseOpenAI._stream")
+            unwrap("langchain_community.llms.openai", "BaseOpenAI._astream")
+            unwrap("langchain_openai.llms.base", "BaseOpenAI._generate")
+            unwrap("langchain_openai.llms.base", "BaseOpenAI._agenerate")
+            unwrap("langchain_openai.llms.base", "BaseOpenAI._stream")
+            unwrap("langchain_openai.llms.base", "BaseOpenAI._astream")
+            unwrap("langchain_openai.chat_models.base", "BaseOpenAI._generate")
+            unwrap("langchain_openai.chat_models.base", "BaseOpenAI._agenerate")
+            # unwrap("langchain_openai.chat_models.base", "BaseOpenAI._stream")
+            # unwrap("langchain_openai.chat_models.base", "BaseOpenAI._astream")
 
 
 class _BaseCallbackManagerInitWrapper:
@@ -63,3 +163,40 @@ class _BaseCallbackManagerInitWrapper:
                 break
         else:
             instance.add_handler(self._callback_manager, True)
+
+
+# This class wraps a function call to inject tracing information (trace headers) into
+# OpenAI client requests. It assumes the following:
+# 1. The wrapped function includes a `run_manager` keyword argument that contains a `run_id`.
+#    The `run_id` is used to look up a corresponding tracing span from the callback manager.
+# 2. The `kwargs` passed to the wrapped function are forwarded to the OpenAI client. This
+#    allows us to add extra headers (including tracing headers) to the OpenAI request by
+#    modifying the `extra_headers` argument in `kwargs`.
+class _OpenAITracingWrapper:
+    def __init__(self, callback_manager: "TraceloopCallbackHandler"):
+        self._callback_manager = callback_manager
+
+    def __call__(
+        self,
+        wrapped,
+        instance,
+        args,
+        kwargs,
+    ) -> None:
+
+        run_manager = kwargs.get("run_manager")
+        if run_manager:
+            run_id = run_manager.run_id
+            span_holder = self._callback_manager.spans[run_id]
+
+            extra_headers = kwargs.get("extra_headers", {})
+
+            # Inject tracing context into the extra headers
+            ctx = set_span_in_context(span_holder.span)
+            TraceContextTextMapPropagator().inject(extra_headers, context=ctx)
+            # TraceContextTextMapPropagator().inject(extra_headers, context=span_holder.context)
+
+            # Update kwargs to include the modified headers
+            kwargs["extra_headers"] = extra_headers
+
+        return wrapped(*args, **kwargs)
