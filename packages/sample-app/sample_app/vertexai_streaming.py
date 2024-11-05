2451bf67c7ac70d67c8252ce0479e2a47138ae07
Nir Gazit <nirga@users.noreply.github.com>
2024-11-05 15:26:11 +0100

    fix(vertex): async / streaming was missing output fields

--- packages/sample-app/sample_app/vertexai_streaming.py
+++ packages/sample-app/sample_app/vertexai_streaming.py
@@ -1,6 +1,7 @@
+import asyncio
 import vertexai
 from traceloop.sdk import Traceloop
-from traceloop.sdk.decorators import workflow
+from traceloop.sdk.decorators import aworkflow
 from vertexai.generative_models import GenerativeModel
 
 Traceloop.init(app_name="stream_prediction_service")
@@ -8,8 +9,8 @@ Traceloop.init(app_name="stream_prediction_service")
 vertexai.init()
 
 
-@workflow("stream_prediction")
-def streaming_prediction() -> str:
+@aworkflow("stream_prediction")
+async def streaming_prediction() -> str:
     """Streaming Text Example with a Large Language Model"""
 
     model = GenerativeModel(
@@ -27,10 +28,10 @@ def streaming_prediction() -> str:
 
     contents = [prompt]
 
-    response = model.generate_content(contents)
-
-    return response.text
+    response = await model.generate_content_async(contents, stream=True)
+    async for chunk in response:
+        print(chunk.text)
 
 
 if __name__ == "__main__":
-    print(streaming_prediction())
+    asyncio.run(streaming_prediction())
