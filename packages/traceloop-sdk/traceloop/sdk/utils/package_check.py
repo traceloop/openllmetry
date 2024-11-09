a597b8d3788c79c9fbdbd9356f53b876014d2c28
Nir Gazit <nirga@users.noreply.github.com>
2024-11-09 09:41:12 +0100

    fix(sdk): remove print

--- packages/traceloop-sdk/traceloop/sdk/utils/package_check.py
+++ packages/traceloop-sdk/traceloop/sdk/utils/package_check.py
@@ -2,8 +2,6 @@ from importlib.metadata import distributions
 
 installed_packages = {dist.metadata["Name"].lower() for dist in distributions()}
 
-print(installed_packages)
-
 
 def is_package_installed(package_name: str) -> bool:
     return package_name.lower() in installed_packages
