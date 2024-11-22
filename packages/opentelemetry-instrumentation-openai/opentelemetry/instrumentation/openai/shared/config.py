19360d44e07df679612f315534531b6a57030d4f
Ronen Schaffer <ronen.schaffer@ibm.com>
2024-11-22 08:36:40 +0200

    fix(openai): Add trace context to client requests (#2321)

--- packages/opentelemetry-instrumentation-openai/opentelemetry/instrumentation/openai/shared/config.py
+++ packages/opentelemetry-instrumentation-openai/opentelemetry/instrumentation/openai/shared/config.py
@@ -7,3 +7,4 @@ class Config:
     exception_logger = None
     get_common_metrics_attributes: Callable[[], dict] = lambda: {}
     upload_base64_image: Callable[[str, str, str], str] = lambda trace_id, span_id, base64_image_url: str
+    enable_trace_context_propagation: bool = True
