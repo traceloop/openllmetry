4afbe632bdb70926049b8f19d66810a20fb3f3ae
Adharsh H <Adharsh.H@ibm.com>
2025-01-03 15:20:03 +0530

    Modified the instrumentation

--- packages/opentelemetry-instrumentation-crewai/opentelemetry/instrumentation/crewai/instrumentation.py
+++ packages/opentelemetry-instrumentation-crewai/opentelemetry/instrumentation/crewai/instrumentation.py
@@ -5,24 +5,25 @@ from opentelemetry.trace import SpanKind, get_tracer
 from opentelemetry.trace.status import Status, StatusCode
 from opentelemetry.instrumentation.crewai.version import __version__
 from opentelemetry.semconv_ai import SpanAttributes, TraceloopSpanKindValues
+from .crewai_span_attributes import CrewAISpanAttributes
 
 _instruments = ("crewai >= 0.70.0",)
 
 class CrewAIInstrumentor(BaseInstrumentor):
-    
-    
+
+
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
-    
-    
+
+
     def _instrument(self, **kwargs):
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(__name__, __version__, tracer_provider)
 
-        wrap_function_wrapper("crewai", "Crew.kickoff", wrap_kickoff(tracer))
+        wrap_function_wrapper("crewai.crew", "Crew.kickoff", wrap_kickoff(tracer))
         wrap_function_wrapper("crewai.agent", "Agent.execute_task", wrap_agent_execute_task(tracer))
+        wrap_function_wrapper("crewai.task", "Task.execute_sync", wrap_task_execute(tracer))
         wrap_function_wrapper("crewai.tool", "Tool.run", wrap_tool_run(tracer))
-        # wrap_function_wrapper("crewai.task", "Task.execute", wrap_execute_task(tracer))
 
     
     def _uninstrument(self, **kwargs):
@@ -43,13 +44,15 @@ def with_tracer_wrapper(func):
 def wrap_kickoff(tracer, wrapped, instance, args, kwargs):
     with tracer.start_as_current_span(
         "crewai.workflow",
-        kind=SpanKind.INTERNAL,
+        kind=SpanKind.CLIENT,
         attributes={
         }
     ) as span:
         try:
+            CrewAISpanAttributes(span=span, instance=instance)
             result = wrapped(*args, **kwargs)
-            span.set_status(Status(StatusCode.OK))
+            if result:
+                update_span_with_result(span, instance, result)
             return result
         except Exception as ex:
             span.set_status(Status(StatusCode.ERROR, str(ex)))
@@ -62,14 +65,37 @@ def wrap_agent_execute_task(tracer, wrapped, instance, args, kwargs):
 
     with tracer.start_as_current_span(
         f"{agent_name}.agent",
-        kind=SpanKind.INTERNAL,
+        kind=SpanKind.CLIENT,
         attributes={
             SpanAttributes.TRACELOOP_SPAN_KIND: TraceloopSpanKindValues.AGENT.value,
         }
     ) as span:
         try:
+            CrewAISpanAttributes(span=span, instance=instance)
             result = wrapped(*args, **kwargs)
-            span.set_status(Status(StatusCode.OK))
+            if result:
+                update_span_with_result(span, instance, result)
+            return result
+        except Exception as ex:
+            span.set_status(Status(StatusCode.ERROR, str(ex)))
+            raise 
+        
+@with_tracer_wrapper
+def wrap_task_execute(tracer, wrapped, instance, args, kwargs):
+    task_name = instance.description if hasattr(instance, "description") else "task"
+
+    with tracer.start_as_current_span(
+        f"Task.execute.{task_name}",
+        kind=SpanKind.CLIENT,
+        attributes={
+            SpanAttributes.TRACELOOP_SPAN_KIND: TraceloopSpanKindValues.TASK.value,
+        }
+    ) as span:
+        try:
+            CrewAISpanAttributes(span=span, instance=instance)
+            result = wrapped(*args, **kwargs)
+            if result:
+                update_span_with_result(span, instance, result)
             return result
         except Exception as ex:
             span.set_status(Status(StatusCode.ERROR, str(ex)))
@@ -94,3 +120,12 @@ def wrap_tool_run(tracer, wrapped, instance, args, kwargs):
         except Exception as ex:
             span.set_status(Status(StatusCode.ERROR, str(ex)))
             raise 
+        
+def update_span_with_result(span, instance, result):
+    class_name = instance.__class__.__name__
+    span.set_attribute(f"crewai.{class_name.lower()}.result", str(result))
+    span.set_status(Status(StatusCode.OK))
+    if class_name == "Crew":
+        for attr in ["tasks_output", "token_usage", "usage_metrics"]:
+            if hasattr(result, attr):
+                span.set_attribute(f"crewai.crew.{attr}", str(getattr(result, attr)))
