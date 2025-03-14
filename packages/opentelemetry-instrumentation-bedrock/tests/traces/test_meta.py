1037d1da8c6adde8a59e9bcb7ba2c705e9404340
Giovanni Liva <giovanni.liva@dynatrace.com>
2025-03-14 15:32:39 +0100

    feat(instrumentation): Support Converse APIs and guardrail metrics (#2725)
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-bedrock/tests/traces/test_meta.py
+++ packages/opentelemetry-instrumentation-bedrock/tests/traces/test_meta.py
@@ -82,3 +82,145 @@ def test_meta_llama3_completion(test_context, brt):
         == response_body["generation"]
     )
     assert meta_span.attributes.get("gen_ai.response.id") is None
+
+
+@pytest.mark.vcr
+def test_meta_converse(test_context, brt):
+    model_id = "meta.llama3-2-1b-instruct-v1:0"
+    inference_config = {"temperature": 0.5}
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "text": "Tell me a joke about opentelemetry"
+                },
+            ],
+        }
+    ]
+    system_prompt = "You are an app that knows about everything."
+    system = [{"text": system_prompt}]
+    response = brt.converse(
+        modelId=model_id,
+        messages=messages,
+        system=system,
+        inferenceConfig=inference_config,
+    )
+
+    generated_text = response["output"]["message"]["content"]
+
+    exporter, _, _ = test_context
+    spans = exporter.get_finished_spans()
+    assert all(span.name == "bedrock.converse" for span in spans)
+
+    meta_span = spans[0]
+    assert (
+            meta_span.attributes[SpanAttributes.LLM_USAGE_PROMPT_TOKENS]
+            == response["usage"]['inputTokens']
+    )
+    assert (
+            meta_span.attributes[SpanAttributes.LLM_USAGE_COMPLETION_TOKENS]
+            == response["usage"]['outputTokens']
+    )
+    assert (
+            meta_span.attributes[SpanAttributes.LLM_USAGE_TOTAL_TOKENS]
+            == response["usage"]['totalTokens']
+    )
+    assert meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.0.role"] == "system"
+    assert (
+            meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.0.content"]
+            == system_prompt
+    )
+    assert meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.role"] == "user"
+    assert (
+            meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.content"]
+            == json.dumps(messages[0]["content"])
+    )
+    for i in range(0, len(generated_text)):
+        assert meta_span.attributes[f"{SpanAttributes.LLM_COMPLETIONS}.{i}.role"] == "assistant"
+        assert (
+                meta_span.attributes[f"{SpanAttributes.LLM_COMPLETIONS}.{i}.content"]
+                == generated_text[i]["text"]
+        )
+    assert meta_span.attributes.get("gen_ai.response.id") is None
+
+
+@pytest.mark.vcr
+def test_meta_converse_stream(test_context, brt):
+    model_id = "meta.llama3-2-1b-instruct-v1:0"
+    inference_config = {"temperature": 0.5}
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "text": "Tell me a joke about opentelemetry"
+                },
+            ],
+        }
+    ]
+    system_prompt = "You are an app that knows about everything."
+    system = [{"text": system_prompt}]
+    response = brt.converse_stream(
+        modelId=model_id,
+        messages=messages,
+        system=system,
+        inferenceConfig=inference_config,
+    )
+
+    stream = response.get("stream")
+
+    response_role = None
+    content = ""
+    inputTokens = 0
+    outputTokens = 0
+
+    if stream:
+        for event in stream:
+
+            if "messageStart" in event:
+                response_role = event["messageStart"]["role"]
+
+            if "contentBlockDelta" in event:
+                content += event["contentBlockDelta"]["delta"]["text"]
+
+            if "metadata" in event:
+                metadata = event["metadata"]
+                if "usage" in metadata:
+                    inputTokens = metadata["usage"]["inputTokens"]
+                    outputTokens = metadata["usage"]["outputTokens"]
+
+    exporter, _, _ = test_context
+    spans = exporter.get_finished_spans()
+    assert all(span.name == "bedrock.converse" for span in spans)
+
+    meta_span = spans[0]
+    assert (
+            meta_span.attributes[SpanAttributes.LLM_USAGE_PROMPT_TOKENS]
+            == inputTokens
+    )
+    assert (
+            meta_span.attributes[SpanAttributes.LLM_USAGE_COMPLETION_TOKENS]
+            == outputTokens
+    )
+    assert (
+            meta_span.attributes[SpanAttributes.LLM_USAGE_TOTAL_TOKENS]
+            == inputTokens + outputTokens
+    )
+    assert meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.0.role"] == "system"
+    assert (
+            meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.0.content"]
+            == system_prompt
+    )
+    assert meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.role"] == "user"
+    assert (
+            meta_span.attributes[f"{SpanAttributes.LLM_PROMPTS}.1.content"]
+            == json.dumps(messages[0]["content"])
+    )
+
+    assert meta_span.attributes[f"{SpanAttributes.LLM_COMPLETIONS}.0.role"] == response_role
+    assert (
+            meta_span.attributes[f"{SpanAttributes.LLM_COMPLETIONS}.0.content"]
+            == content
+    )
+    assert meta_span.attributes.get("gen_ai.response.id") is None
