077248ffdac1d861dc531c805c6bdd9a88bfc3f8
Giovanni Liva <giovanni.liva@dynatrace.com>
2025-03-24 20:16:58 +0100

    fix(bedrock): add span attr for Bedrock prompt caching (#2789)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-bedrock/opentelemetry/instrumentation/bedrock/prompt_caching.py
+++ packages/opentelemetry-instrumentation-bedrock/opentelemetry/instrumentation/bedrock/prompt_caching.py
@@ -1,3 +1,6 @@
+from opentelemetry import trace
+
+
 class CachingHeaders:
     READ = "x-amzn-bedrock-cache-read-input-token-count"
     WRITE = "x-amzn-bedrock-cache-write-input-token-count"
@@ -5,6 +8,7 @@ class CachingHeaders:
 
 class CacheSpanAttrs:  # TODO: move it under SemConv pkg
     TYPE = "gen_ai.cache.type"
+    CACHED = "gen_ai.prompt_caching"
 
 
 def prompt_caching_handling(headers, vendor, model, metric_params):
@@ -12,6 +16,7 @@ def prompt_caching_handling(headers, vendor, model, metric_params):
         "gen_ai.system": vendor,
         "gen_ai.response.model": model,
     }
+    span = trace.get_current_span()
     if CachingHeaders.READ in headers:
         read_cached_tokens = int(headers[CachingHeaders.READ])
         metric_params.prompt_caching.add(
@@ -21,6 +26,8 @@ def prompt_caching_handling(headers, vendor, model, metric_params):
                 CacheSpanAttrs.TYPE: "read",
             },
         )
+        if read_cached_tokens > 0:
+            span.set_attribute(CacheSpanAttrs.CACHED, "read")
     if CachingHeaders.WRITE in headers:
         write_cached_tokens = int(headers[CachingHeaders.WRITE])
         metric_params.prompt_caching.add(
@@ -30,3 +37,5 @@ def prompt_caching_handling(headers, vendor, model, metric_params):
                 CacheSpanAttrs.TYPE: "write",
             },
         )
+        if write_cached_tokens > 0:
+            span.set_attribute(CacheSpanAttrs.CACHED, "write")
