8c6fc0b37649f5020ef08d3ba1c902cef67fcbb3
Luiz D. M. Mainart <luizdmmainart@gmail.com>
2025-03-27 22:43:28 -0300

    feat(alephalpha): implement the support to emitting events in addition to current behavior
    * Add "use_legacy_attributes" to Config and the Instrumentor constructor, defaulting to True;
    * emit events for user prompts and AI responses, following [OpenTelemetry semantic conventions](https://opentelemetry.io/docs/specs/semconv/gen-ai/gen-ai-events/);
    * introduce a privacy safeguard by checking the OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT environment variable to enable or disable content capture in events;
    * implement comprehensive tests to verify the new functionality and ensure that, when use_legacy_attributes == True, the existing behavior remains unchanged.

--- packages/opentelemetry-instrumentation-alephalpha/opentelemetry/instrumentation/alephalpha/__init__.py
+++ packages/opentelemetry-instrumentation-alephalpha/opentelemetry/instrumentation/alephalpha/__init__.py
@@ -2,27 +2,33 @@
 
 import logging
 import os
-from typing import Collection
-from opentelemetry.instrumentation.alephalpha.config import Config
-from opentelemetry.instrumentation.alephalpha.utils import dont_throw
-from wrapt import wrap_function_wrapper
+from typing import Collection, Union
 
 from opentelemetry import context as context_api
-from opentelemetry.trace import get_tracer, SpanKind
-from opentelemetry.trace.status import Status, StatusCode
-
+from opentelemetry._events import Event, EventLogger, get_event_logger
+from opentelemetry.instrumentation.alephalpha.config import Config
+from opentelemetry.instrumentation.alephalpha.utils import (
+    dont_throw,
+    is_content_enabled,
+)
+from opentelemetry.instrumentation.alephalpha.version import __version__
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.utils import (
     _SUPPRESS_INSTRUMENTATION_KEY,
     unwrap,
 )
-
+from opentelemetry.semconv._incubating.attributes import (
+    gen_ai_attributes as GenAIAttributes,
+)
 from opentelemetry.semconv_ai import (
     SUPPRESS_LANGUAGE_MODEL_INSTRUMENTATION_KEY,
-    SpanAttributes,
     LLMRequestTypeValues,
+    SpanAttributes,
 )
-from opentelemetry.instrumentation.alephalpha.version import __version__
+from opentelemetry.trace import SpanKind, Tracer, get_tracer
+from opentelemetry.trace.span import Span
+from opentelemetry.trace.status import Status, StatusCode
+from wrapt import wrap_function_wrapper
 
 logger = logging.getLogger(__name__)
 
@@ -99,9 +105,9 @@ def _set_response_attributes(span, llm_request_type, response):
 def _with_tracer_wrapper(func):
     """Helper for providing tracer for wrapper functions."""
 
-    def _with_tracer(tracer, to_wrap):
+    def _with_tracer(tracer, event_logger, to_wrap):
         def wrapper(wrapped, instance, args, kwargs):
-            return func(tracer, to_wrap, wrapped, instance, args, kwargs)
+            return func(tracer, event_logger, to_wrap, wrapped, instance, args, kwargs)
 
         return wrapper
 
@@ -115,8 +121,38 @@ def _llm_request_type_by_method(method_name):
         return LLMRequestTypeValues.UNKNOWN
 
 
+def _chat_completion_to_event(span: Span, args, kwargs) -> Event:
+    request = kwargs.get("request") if kwargs.get("request") else args[0]
+
+    attributes = {GenAIAttributes.GEN_AI_SYSTEM: "AlephAlpha"}
+    body = {"content": request.prompt.items} if is_content_enabled() else {}
+
+    return Event(name="gen_ai.user.message", body=body, attributes=attributes)
+
+
+def _completion_to_event(index, completion) -> Event:
+    attributes = {GenAIAttributes.GEN_AI_SYSTEM: "AlephAlpha"}
+
+    body = {"index": index, "finish_reason": completion.finish_reason}
+    if is_content_enabled():
+        message = {"content": completion.completion}
+    else:
+        message = {}
+    body["message"] = message
+
+    return Event(name="gen_ai.choice", body=body, attributes=attributes)
+
+
 @_with_tracer_wrapper
-def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
+def _wrap(
+    tracer: Tracer,
+    event_logger: Union[EventLogger, None],
+    to_wrap,
+    wrapped,
+    instance,
+    args,
+    kwargs,
+):
     """Instruments and calls every function defined in TO_WRAP."""
     if context_api.get_value(_SUPPRESS_INSTRUMENTATION_KEY) or context_api.get_value(
         SUPPRESS_LANGUAGE_MODEL_INSTRUMENTATION_KEY
@@ -135,14 +171,18 @@ def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
     )
     if span.is_recording():
         _set_input_attributes(span, llm_request_type, args, kwargs)
+    if event_logger is not None:
+        event_logger.emit(_chat_completion_to_event(span, args, kwargs))
 
     response = wrapped(*args, **kwargs)
 
     if response:
         if span.is_recording():
-
             _set_response_attributes(span, llm_request_type, response)
             span.set_status(Status(StatusCode.OK))
+        if event_logger is not None:
+            for index, completion in enumerate(response.completions):
+                event_logger.emit(_completion_to_event(index, completion))
 
     span.end()
     return response
@@ -151,9 +191,10 @@ def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
 class AlephAlphaInstrumentor(BaseInstrumentor):
     """An instrumentor for Aleph Alpha's client library."""
 
-    def __init__(self, exception_logger=None):
+    def __init__(self, exception_logger=None, use_legacy_attributes=True):
         super().__init__()
         Config.exception_logger = exception_logger
+        Config.use_legacy_attributes = use_legacy_attributes
 
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
@@ -161,18 +202,28 @@ class AlephAlphaInstrumentor(BaseInstrumentor):
     def _instrument(self, **kwargs):
         tracer_provider = kwargs.get("tracer_provider")
         tracer = get_tracer(__name__, __version__, tracer_provider)
+
+        if Config.use_legacy_attributes:
+            event_logger = None
+        else:
+            event_logger_provider = kwargs.get("event_logger_provider")
+            event_logger = get_event_logger(
+                __name__,
+                __version__,
+                event_logger_provider=event_logger_provider,
+            )
+
         for wrapped_method in WRAPPED_METHODS:
             wrap_method = wrapped_method.get("method")
             wrap_function_wrapper(
                 "aleph_alpha_client",
                 f"Client.{wrap_method}",
-                _wrap(tracer, wrapped_method),
+                _wrap(tracer, event_logger, wrapped_method),
             )
 
     def _uninstrument(self, **kwargs):
         for wrapped_method in WRAPPED_METHODS:
-            wrap_object = wrapped_method.get("object")
             unwrap(
-                f"aleph_alpha_client.Client.{wrap_object}",
+                "aleph_alpha_client.Client",
                 wrapped_method.get("method"),
             )
