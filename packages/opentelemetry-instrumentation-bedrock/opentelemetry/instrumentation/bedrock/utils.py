7c74ff8b37bbf56c7341dc31fa3689ca3eeda69a
Luiz D. M. Mainart <luizdmmainart@gmail.com>
2025-04-08 00:13:32 -0300

    feat(bedrock): implement the support to emitting events in addition to current behavior
    * Add "use_legacy_attributes" to Config and the Instrumentor constructor, defaulting to True;
    * emit events for user prompts and AI responses, following [OpenTelemetry semantic conventions](https://opentelemetry.io/docs/specs/semconv/gen-ai/gen-ai-events/\\\);
    * introduce a privacy safeguard by checking the OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT environment variable to enable or disable content capture in events;
    * implement comprehensive tests to verify the new functionality and ensure that, when use_legacy_attributes == True, the existing behavior remains unchanged;

--- packages/opentelemetry-instrumentation-bedrock/opentelemetry/instrumentation/bedrock/utils.py
+++ packages/opentelemetry-instrumentation-bedrock/opentelemetry/instrumentation/bedrock/utils.py
@@ -1,7 +1,15 @@
 import logging
+import os
 import traceback
 
 from opentelemetry.instrumentation.bedrock.config import Config
+from opentelemetry.semconv._incubating.attributes import (
+    gen_ai_attributes as GenAIAttributes,
+)
+
+OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT = (
+    "OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT"
+)
 
 
 def dont_throw(func):
@@ -27,3 +35,17 @@ def dont_throw(func):
                 Config.exception_logger(e)
 
     return wrapper
+
+
+def get_event_attributes():
+    return {
+        GenAIAttributes.GEN_AI_SYSTEM: GenAIAttributes.GenAiSystemValues.AWS_BEDROCK.value
+    }
+
+
+def is_content_enabled() -> bool:
+    capture_content = os.environ.get(
+        OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT, "false"
+    )
+
+    return capture_content.lower() == "true"
