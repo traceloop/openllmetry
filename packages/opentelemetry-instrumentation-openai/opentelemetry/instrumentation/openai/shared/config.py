502f1ab2d9e6d9f45d1471812ed3e693fdd1386c
krishna bhagat <krishnabhagat2104@gmail.com>
2025-04-09 15:23:58 +0530

    feat(openai): support emitting prompt and completion events as OpenTelemetry log events

--- packages/opentelemetry-instrumentation-openai/opentelemetry/instrumentation/openai/shared/config.py
+++ packages/opentelemetry-instrumentation-openai/opentelemetry/instrumentation/openai/shared/config.py
@@ -2,9 +2,14 @@ from typing import Callable
 
 
 class Config:
+    use_legacy_attributes = True
+    emit_prompt_events = True
+    emit_completion_events = True
     enrich_token_usage = False
     enrich_assistant = False
     exception_logger = None
     get_common_metrics_attributes: Callable[[], dict] = lambda: {}
-    upload_base64_image: Callable[[str, str, str], str] = lambda trace_id, span_id, base64_image_url: str
+    upload_base64_image: Callable[[str, str, str], str] = (
+        lambda trace_id, span_id, base64_image_url: str
+    )
     enable_trace_context_propagation: bool = True
