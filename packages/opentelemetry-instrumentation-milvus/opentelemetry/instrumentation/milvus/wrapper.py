8c168fc2ed23f22c642b6457f0c934af699d5f5b
Divya Pathak <divya3840@gmail.com>
2025-05-05 09:30:38 +0530

    updated milvus search instrumentation

--- packages/opentelemetry-instrumentation-milvus/opentelemetry/instrumentation/milvus/wrapper.py
+++ packages/opentelemetry-instrumentation-milvus/opentelemetry/instrumentation/milvus/wrapper.py
@@ -55,8 +55,12 @@ def _wrap(tracer, to_wrap, wrapped, instance, args, kwargs):
             _set_create_collection_attributes(span, kwargs)
 
         return_value = wrapped(*args, **kwargs)
+
         if to_wrap.get("method") == "query":
             _add_query_result_events(span, return_value)
+
+        if to_wrap.get("method") == "search":
+            _add_search_result_events(span, return_value)
 
     return return_value
 
@@ -198,7 +202,9 @@ def _set_search_attributes(span, kwargs):
         count_or_none(kwargs.get("output_fields")),
     )
     _set_span_attribute(
-        span, AISpanAttributes.MILVUS_SEARCH_SEARCH_PARAMS, kwargs.get("search_params")
+        span,
+        AISpanAttributes.MILVUS_SEARCH_SEARCH_PARAMS,
+        _encode_include(kwargs.get("search_params")),
     )
     _set_span_attribute(
         span, AISpanAttributes.MILVUS_SEARCH_TIMEOUT, kwargs.get("timeout")
@@ -210,6 +216,19 @@ def _set_search_attributes(span, kwargs):
     )
     _set_span_attribute(
         span, AISpanAttributes.MILVUS_SEARCH_ANNS_FIELD, kwargs.get("anns_field")
+    )
+    _set_span_attribute(
+        span,
+        AISpanAttributes.MILVUS_SEARCH_PARTITION_NAMES,
+        _encode_partition_name(kwargs.get("partition_names")),
+    )
+    query_vectors = kwargs.get("data", [])
+    vector_dims = [len(vec) for vec in query_vectors]
+
+    _set_span_attribute(
+        span,
+        AISpanAttributes.MILVUS_SEARCH_QUERY_VECTOR_DIMENSION,
+        _encode_include(vector_dims),
     )
 
 
@@ -246,6 +265,78 @@ def _set_query_attributes(span, kwargs):
 def _add_query_result_events(span, kwargs):
     for element in kwargs:
         span.add_event(name=Events.DB_QUERY_RESULT.value, attributes=element)
+
+
+@dont_throw
+def _add_search_result_events(span, kwargs):
+
+    all_distances = []
+    total_matches = 0
+
+    single_query = len(kwargs) == 1
+
+    def set_query_stats(query_idx, distances, match_ids):
+        """Helper function to set per-query stats in the span."""
+
+        _set_span_attribute(
+            span,
+            f"{AISpanAttributes.MILVUS_SEARCH_RESULT_COUNT}_{query_idx}",
+            len(distances),
+        )
+        _set_span_attribute(
+            span,
+            f"{AISpanAttributes.MILVUS_SEARCH_RESULT_DISTANCES}_{query_idx}",
+            _encode_include(distances),
+        )
+        _set_span_attribute(
+            span,
+            f"{AISpanAttributes.MILVUS_SEARCH_RESULT_TOP_IDS}_{query_idx}",
+            _encode_include(match_ids),
+        )
+
+    def set_global_stats():
+        """Helper function to set global stats for a single query."""
+        _set_span_attribute(
+            span, AISpanAttributes.MILVUS_SEARCH_RESULT_COUNT, total_matches
+        )
+        _set_span_attribute(
+            span,
+            AISpanAttributes.MILVUS_SEARCH_RESULT_DISTANCES,
+            _encode_include(all_distances),
+        )
+        _set_span_attribute(
+            span,
+            AISpanAttributes.MILVUS_SEARCH_RESULT_TOP_IDS,
+            _encode_include(query_match_ids),
+        )
+
+    for query_idx, query_results in enumerate(kwargs):
+
+        query_distances = []
+        query_match_ids = []
+
+        for match in query_results:
+            distance = float(match["distance"])
+            query_distances.append(distance)
+            all_distances.append(distance)
+            total_matches += 1
+            query_match_ids.append(match["id"])
+
+            span.add_event(
+                Events.DB_SEARCH_RESULT.value,
+                attributes={
+                    "query_index": query_idx,
+                    "id": match["id"],
+                    "distance": str(distance),
+                    "entity": _encode_include(match["entity"]),
+                },
+            )
+
+        if not single_query:
+            set_query_stats(query_idx, query_distances, query_match_ids)
+
+    if single_query:
+        set_global_stats()
 
 
 @dont_throw
