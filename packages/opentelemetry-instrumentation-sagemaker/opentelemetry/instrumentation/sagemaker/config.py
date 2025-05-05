7c4df5148f050cd7d9b557c01713055bc05e64f2
Luiz D. M. Mainart <luizdmmainart@gmail.com>
2025-05-05 18:14:56 -0300

    feat(sagemaker): add event_handler module and refactor events logic
    - Introduce TypedDicts and dataclasses to standardize inputs for event handling;
    - Move `event_logger` to Config to centralize configuration and reduce complexity;
    - Add `emit_event` method to encapsulate all event emission logic via `Event` instances;
    - Refactor instrumentation to use `emit_event`, improving clarity and maintainability.

--- packages/opentelemetry-instrumentation-sagemaker/opentelemetry/instrumentation/sagemaker/config.py
+++ packages/opentelemetry-instrumentation-sagemaker/opentelemetry/instrumentation/sagemaker/config.py
@@ -1,4 +1,10 @@
+from typing import Optional
+
+from opentelemetry._events import EventLogger
+
+
 class Config:
     enrich_token_usage = False
     exception_logger = None
     use_legacy_attributes = True
+    event_logger: Optional[EventLogger] = None
