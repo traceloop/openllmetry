a40b28b02a195908362341d96ca19efd29a0a0a7
Divya Pathak <42262349+divyapathak24@users.noreply.github.com>
2025-05-10 00:48:56 +0530

    fix(milvus): Added New Semantic Conventions for Milvus Search (Request for Version Update 0.4.5 -> 0.4.6) (#2883)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-semantic-conventions-ai/opentelemetry/semconv_ai/__init__.py
+++ packages/opentelemetry-semantic-conventions-ai/opentelemetry/semconv_ai/__init__.py
@@ -195,6 +195,9 @@ class SpanAttributes:
     MILVUS_SEARCH_PARTITION_NAMES_COUNT = "db.milvus.search.partition_names_count"
     MILVUS_SEARCH_SEARCH_PARAMS = "db.milvus.search.search_params"
     MILVUS_SEARCH_TIMEOUT = "db.milvus.search.timeout"
+    MILVUS_SEARCH_PARTITION_NAMES = "db.milvus.search.partition_names_count"
+    MILVUS_SEARCH_RESULT_COUNT = "db.milvus.search.result_count"
+    MILVUS_SEARCH_QUERY_VECTOR_DIMENSION = "db.milvus.search.query_vector_dimension"
     MILVUS_UPSERT_COLLECTION_NAME = "db.milvus.upsert.collection_name"
     MILVUS_UPSERT_DATA_COUNT = "db.milvus.upsert.data_count"
     MILVUS_UPSERT_PARTITION_NAME = "db.milvus.upsert.partition_name"
@@ -225,6 +228,8 @@ class SpanAttributes:
 class Events(Enum):
     DB_QUERY_EMBEDDINGS = "db.query.embeddings"
     DB_QUERY_RESULT = "db.query.result"
+    DB_SEARCH_EMBEDDINGS = "db.search.embeddings"
+    DB_SEARCH_RESULT = "db.search.result"
 
 
 class EventAttributes(Enum):
@@ -238,6 +243,15 @@ class EventAttributes(Enum):
     DB_QUERY_RESULT_METADATA = "db.query.result.metadata"
     DB_QUERY_RESULT_VECTOR = "db.query.result.vector"
     DB_QUERY_RESULT_DOCUMENT = "db.query.result.document"
+
+    # SEARCH
+    DB_SEARCH_EMBEDDINGS_VECTOR = "db.search.embeddings.vector"
+
+    DB_SEARCH_RESULT_QUERY_ID = "db.search.query.id"  # For multi-vector searches
+    DB_SEARCH_RESULT_ID = "db.search.result.id"
+    DB_SEARCH_RESULT_SCORE = "db.search.result.score"
+    DB_SEARCH_RESULT_DISTANCE = "db.search.result.distance"
+    DB_SEARCH_RESULT_ENTITY = "db.search.result.entity"
 
 
 class LLMRequestTypeValues(Enum):
