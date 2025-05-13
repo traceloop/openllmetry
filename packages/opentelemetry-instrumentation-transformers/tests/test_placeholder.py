b5a43cbf201915537089352d2768a150860db248
Luiz D. M. Mainart <luizdmmainart@gmail.com>
2025-05-12 22:06:38 -0300

    feat(transformers): implement the support to emitting events
    - Implement event emition logic in addition to current behavior;
    - Add "use_legacy_attributes" to Config and the Instrumentor constructor, defaulting to True;
    - emit events for user prompts and AI responses, following [OpenTelemetry semantic conventions](https://opentelemetry.io/docs/specs/semconv/gen-ai/gen-ai-events/);
    - fix prompt span attribute settings;
    - implement testing framework.

--- packages/opentelemetry-instrumentation-transformers/tests/test_placeholder.py
+++ packages/opentelemetry-instrumentation-transformers/tests/test_placeholder.py
@@ -1,2 +1,12 @@
+import transformers
+
+
 def test_placeholder():
-    pass
+    pipeline = transformers.pipeline(
+        task="text-generation", model="gpt2", framework="tf"
+    )
+    response = pipeline("the secret to baking a really good cake is ")
+    print(type(response))
+    print(response[0])
+    print(type(response[0]))
+    raise AssertionError
