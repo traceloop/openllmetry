f045353b824ac27ed8ff16a5d16af8138818cc4b
Adharsh <adharshctr@gmail.com>
2025-05-29 13:39:09 +0530

    Added a sample app openai agent using litellm

---
+++ packages/sample-app/sample_app/openai_agents_using_litellm.py
@@ -0,0 +1,36 @@
+import asyncio
+from agents import Agent, Runner
+from agents.extensions.models.litellm_model import LitellmModel
+from agents import ModelSettings
+from traceloop.sdk import Traceloop
+
+Traceloop.init()
+
+useModel = LitellmModel(
+    model="groq/llama3-70b-8192",
+    api_key="<Groq API Key>",
+)
+
+
+agent = Agent(
+    name="GroqAgent",
+    instructions="You are a helpful assistant that answers all questions",
+    model=useModel,
+    model_settings=ModelSettings(
+        temperature=0.3, max_tokens=1024, top_p=0.2, frequency_penalty=0.3
+    ),
+)
+
+
+async def main():
+
+    result = await Runner.run(
+        starting_agent=agent,
+        input="What is quantum computing?",
+    )
+    summary = result.final_output
+    print("\n Output:\n", summary)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
