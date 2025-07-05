e8f387ad9bb0c0a4b1b3e2a9cbb1adabc3154551
Luiz D. M. Mainart <55499897+LuizDMM@users.noreply.github.com>
2025-07-05 11:56:58 -0300

    feat(watsonx): implement emitting events in addition to current behavior (#2896)
    Co-authored-by: Nir Gazit <nirga@users.noreply.github.com>

--- packages/opentelemetry-instrumentation-watsonx/tests/metrics/test_watsonx_metrics.py
+++ packages/opentelemetry-instrumentation-watsonx/tests/metrics/test_watsonx_metrics.py
@@ -8,12 +8,12 @@ from opentelemetry.semconv_ai import Meters, SpanAttributes
     sys.version_info < (3, 10), reason="ibm-watson-ai requires python3.10"
 )
 @pytest.mark.vcr
-def test_generate_metrics(metrics_test_context, watson_ai_model):
+def test_generate_metrics(metrics_test_context_legacy, watson_ai_model, log_exporter):
     if watson_ai_model is None:
         print("test_generate_metrics test skipped.")
         return
 
-    provider, reader = metrics_test_context
+    provider, reader = metrics_test_context_legacy
 
     watson_ai_model.generate(prompt="What is 1 + 1?")
 
@@ -28,7 +28,6 @@ def test_generate_metrics(metrics_test_context, watson_ai_model):
     for rm in resource_metrics:
         for sm in rm.scope_metrics:
             for metric in sm.metrics:
-
                 if metric.name == Meters.LLM_TOKEN_USAGE:
                     found_token_metric = True
                     for data_point in metric.data.data_points:
@@ -61,17 +60,24 @@ def test_generate_metrics(metrics_test_context, watson_ai_model):
     assert found_response_metric is True
     assert found_duration_metric is True
 
+    logs = log_exporter.get_finished_logs()
+    assert (
+        len(logs) == 0
+    ), "Assert that it doesn't emit logs when use_legacy_attributes is True"
+
 
 @pytest.mark.skipif(
     sys.version_info < (3, 10), reason="ibm-watson-ai requires python3.10"
 )
 @pytest.mark.vcr
-def test_generate_stream_metrics(metrics_test_context, watson_ai_model):
+def test_generate_stream_metrics(
+    metrics_test_context_legacy, watson_ai_model, log_exporter
+):
     if watson_ai_model is None:
         print("test_generate_stream_metrics test skipped.")
         return
 
-    provider, reader = metrics_test_context
+    provider, reader = metrics_test_context_legacy
 
     response = watson_ai_model.generate_text_stream(
         prompt="Write an epigram about the sun"
@@ -91,7 +97,6 @@ def test_generate_stream_metrics(metrics_test_context, watson_ai_model):
     for rm in resource_metrics:
         for sm in rm.scope_metrics:
             for metric in sm.metrics:
-
                 if metric.name == Meters.LLM_TOKEN_USAGE:
                     found_token_metric = True
                     for data_point in metric.data.data_points:
@@ -123,3 +128,8 @@ def test_generate_stream_metrics(metrics_test_context, watson_ai_model):
     assert found_token_metric is True
     assert found_response_metric is True
     assert found_duration_metric is True
+
+    logs = log_exporter.get_finished_logs()
+    assert (
+        len(logs) == 0
+    ), "Assert that it doesn't emit logs when use_legacy_attributes is True"
