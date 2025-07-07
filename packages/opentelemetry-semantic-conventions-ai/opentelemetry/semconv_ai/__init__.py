6672ae9f95406aceaa13eae7322c2cefcbd114f9
DrishyaDas <drishyamkd@gmail.com>
2025-07-07 09:05:04 +0530

    Update to generic vectordb metrics sem conv

--- packages/opentelemetry-semantic-conventions-ai/opentelemetry/semconv_ai/__init__.py
+++ packages/opentelemetry-semantic-conventions-ai/opentelemetry/semconv_ai/__init__.py
@@ -21,11 +21,11 @@ class Meters:
     PINECONE_DB_USAGE_READ_UNITS = "db.pinecone.usage.read_units"
     PINECONE_DB_USAGE_WRITE_UNITS = "db.pinecone.usage_write_units"
 
-    MILVUS_DB_QUERY_DURATION = "db.milvus.query.duration"
-    MILVUS_DB_SEARCH_DISTANCE = "db.milvus.search.distance"
-    MILVUS_DB_USAGE_INSERT_UNITS = "db.milvus.usage.insert_units"
-    MILVUS_DB_USAGE_UPSERT_UNITS = "db.milvus.usage.upsert_units"
-    MILVUS_DB_USAGE_DELETE_UNITS = "db.milvus.usage.delete_units"
+    VECTOR_DB_QUERY_DURATION = "vectordb.query.duration"
+    VECTOR_DB_SEARCH_DISTANCE = "vectordb.search.distance"
+    VECTOR_DB_USAGE_INSERT_UNITS = "vectordb.usage.insert_units"
+    VECTOR_DB_USAGE_UPSERT_UNITS = "vectordb.usage.upsert_units"
+    VECTOR_DB_USAGE_DELETE_UNITS = "vectordb.usage.delete_units"
 
     LLM_WATSONX_COMPLETIONS_DURATION = "llm.watsonx.completions.duration"
     LLM_WATSONX_COMPLETIONS_EXCEPTIONS = "llm.watsonx.completions.exceptions"
