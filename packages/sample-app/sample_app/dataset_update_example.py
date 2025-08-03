660058ab6a2f0dd2708fe6b5b0daa78c153735d2
Nina Kollman <ninakollman@Ninas-MacBook-Pro.local>
2025-08-03 14:38:28 +0300

    get all

--- packages/sample-app/sample_app/dataset_update_example.py
+++
@@ -1,9 +0,0 @@
-"""
-Example script demonstrating the Traceloop Dataset functionality
-"""
-import os
-import tempfile
-from traceloop.sdk import Traceloop
-from traceloop.sdk.datasets import Dataset, ColumnType
-import pandas as pd
-
