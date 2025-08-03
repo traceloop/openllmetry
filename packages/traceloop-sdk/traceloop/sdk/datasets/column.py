e0a5246835db58cafabf2d6e97e5011661acc634
Nina Kollman <ninakollman@Ninas-MacBook-Pro.local>
2025-08-03 10:01:54 +0300

    wip

--- packages/traceloop-sdk/traceloop/sdk/datasets/column.py
+++ packages/traceloop-sdk/traceloop/sdk/datasets/column.py
@@ -4,7 +4,7 @@ from pydantic import PrivateAttr
 from .base import DatasetBaseModel, ColumnType
 
 if TYPE_CHECKING:
-    from .client import DatasetClient
+    from .dataset import Dataset
 
 
 class Column(DatasetBaseModel):
@@ -13,20 +13,20 @@ class Column(DatasetBaseModel):
     type: ColumnType
     config: Optional[Dict[str, Any]] = None
     dataset_id: str
-    _client: Optional["DatasetClient"] = PrivateAttr(default=None)
+    _client: Optional["Dataset"] = PrivateAttr(default=None)
 
     def delete(self) -> None:
         """Remove this column from dataset"""
         if self._client is None:
-            from .client import DatasetClient
-            self._client = DatasetClient()
-        self._client.delete_column(self.dataset_id, self.id)
+            from .dataset import Dataset
+            self._client = Dataset()
+        self._client.delete_column_api(self.dataset_id, self.id)
 
     def update(self, name: Optional[str] = None, config: Optional[Dict[str, Any]] = None) -> None:
         """Update this column's properties"""
         if self._client is None:
-            from .client import DatasetClient
-            self._client = DatasetClient()
+            from .dataset import Dataset
+            self._client = Dataset()
         
         update_data = {}
         if name is not None:
@@ -37,4 +37,4 @@ class Column(DatasetBaseModel):
             self.config = config
             
         if update_data:
-            self._client.update_column(self.dataset_id, self.id, **update_data)
+            self._client.update_column_api(self.dataset_id, self.id, **update_data)
