f0b522ef5ddc230fb729a5670615fa59f9459df4
Nina Kollman <ninakollman@Ninas-MacBook-Pro.local>
2025-08-18 14:38:39 +0300

    wip

--- packages/traceloop-sdk/traceloop/sdk/evaluator/evaluator.py
+++ packages/traceloop-sdk/traceloop/sdk/evaluator/evaluator.py
@@ -87,3 +87,4 @@ class Evaluator:
         )
         return sse_result
     
+    
--- packages/traceloop-sdk/traceloop/sdk/experiment/experiment.py
+++ packages/traceloop-sdk/traceloop/sdk/experiment/experiment.py
@@ -34,3 +34,4 @@ class Experiment():
         """Create a new experiment context"""
         return ExperimentContext(self)
     
+    
