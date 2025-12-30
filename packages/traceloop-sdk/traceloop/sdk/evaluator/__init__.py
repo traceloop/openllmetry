0265e2a750ce57714e62c38067a18cc9bcea8971
Gal Zilberstein <48966615+galzilber@users.noreply.github.com>
2025-12-30 15:37:38 +0200

    fx

--- packages/traceloop-sdk/traceloop/sdk/evaluator/__init__.py
+++ packages/traceloop-sdk/traceloop/sdk/evaluator/__init__.py
@@ -1,9 +1,10 @@
 from .evaluator import Evaluator
 from .config import EvaluatorDetails
-from .evaluators_made_by_traceloop import EvaluatorMadeByTraceloop
+from .evaluators_made_by_traceloop import EvaluatorMadeByTraceloop, create_evaluator
 
 __all__ = [
     "Evaluator",
     "EvaluatorDetails",
     "EvaluatorMadeByTraceloop",
+    "create_evaluator",
 ]
