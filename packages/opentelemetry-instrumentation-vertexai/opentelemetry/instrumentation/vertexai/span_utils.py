131f7e0d2ad93fec3e31292bb644354f400df858
Dvir Rezenman <dvir@traceloop.com>
2026-06-16 12:18:30 +0300

    use opentel semconv cache attributes consts

--- packages/opentelemetry-instrumentation-vertexai/opentelemetry/instrumentation/vertexai/span_utils.py
+++ packages/opentelemetry-instrumentation-vertexai/opentelemetry/instrumentation/vertexai/span_utils.py
@@ -318,6 +318,6 @@ def set_model_response_attributes(span, llm_model, token_usage):
         if cached_content_token_count is not None:
             _set_span_attribute(
                 span,
-                SpanAttributes.GEN_AI_USAGE_CACHE_READ_INPUT_TOKENS,
+                GenAIAttributes.GEN_AI_USAGE_CACHE_READ_INPUT_TOKENS,
                 cached_content_token_count,
             )
